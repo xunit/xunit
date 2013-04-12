578c9feee913c081eee3f83e45c70a63d2003eb3
Brad Wilson <dotnetguy@gmail.com>
2013-04-12 16:32:02 -0700

    Additional NSubstitute extension methods

--- test2/test.utility/NSubstituteExtensions.cs
+++ test2/test.utility/NSubstituteExtensions.cs
@@ -1,8 +1,66 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using NSubstitute;
 using NSubstitute.Core;
 
 public static class NSubstituteExtensions
 {
+    public static CallInfo Captured<T>(this T substitute, Expression<Action<T>> expr)
+    {
+        return Captured<T>(substitute, 0, expr);
+    }
+
+    public static CallInfo Captured<T>(this T substitute, int callNumber, Expression<Action<T>> expr)
+    {
+        var router = SubstitutionContext.Current.GetCallRouterFor(substitute);
+
+        var method = ExtractMethodInfo(expr);
+
+        var call = router.ReceivedCalls()
+            .Where(c => c.GetMethodInfo() == method)
+            .ElementAtOrDefault(callNumber);
+
+        if (call == null)
+            throw new Exception("Cannot find matching call.");
+
+        var methodParameters = call.GetParameterInfos();
+        var arguments = new Argument[methodParameters.Length];
+        var argumentValues = call.GetArguments();
+
+        for (int i = 0; i < arguments.Length; i++)
+        {
+            var methodParameter = methodParameters[i];
+            var argumentIndex = i;
+
+            arguments[i] = new Argument(methodParameter.ParameterType, () => argumentValues[argumentIndex], _ => { });
+        }
+
+        return new CallInfo(arguments);
+    }
+
+    static MethodInfo ExtractMethodInfo<T>(Expression<Action<T>> expr)
+    {
+        if (expr.Body.NodeType == ExpressionType.Call)
+        {
+            return ((MethodCallExpression)expr.Body).Method;
+        }
+
+        throw new Exception("Cannot find method.");
+    }
+
+    public static void Returns<T>(this Task<T> instance, T value)
+    {
+        instance.Returns(Task.FromResult<T>(value));
+    }
+
+    public static void ReturnsForAnyArgs<T>(this Task<T> instance, T value)
+    {
+        instance.ReturnsForAnyArgs(Task.FromResult<T>(value));
+    }
+
     public static WhenCalledAny<T> WhenAny<T>(this T substitute, Action<T> substituteCall) where T : class
     {
         var context = SubstitutionContext.Current;
@@ -15,9 +73,9 @@ public static class NSubstituteExtensions
         public WhenCalledAny(ISubstitutionContext context, T substitute, Action<T> call, MatchArgs matchArgs)
             : base(context, substitute, call, matchArgs) { }
 
-        public void Do<T>(Action<T> callbackWithArguments)
+        public void Do<T1>(Action<T1> callbackWithArguments)
         {
-            this.Do(callInfo => callbackWithArguments((T)callInfo[0]));
+            this.Do(callInfo => callbackWithArguments((T1)callInfo[0]));
         }
 
         public void Do<T1, T2>(Action<T1, T2> callbackWithArguments)
