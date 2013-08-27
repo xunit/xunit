32934b2b64c436ffbf99781a3d1a32df7c3689be
Brad Wilson <dotnetguy@gmail.com>
2013-08-27 12:06:12 -0700

    Better arg construction in build.ps1 (danke, @rbartelink)

--- build.ps1
+++ build.ps1
@@ -2,16 +2,12 @@ param(
     [string]$target = "Test"
 )
 
-$msbuilds = @(get-command msbuild)
+$msbuilds = @(get-command msbuild -ea SilentlyContinue)
 if ($msbuilds.Count -eq 0) {
     $msbuild = join-path $env:windir "Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 } else {
     $msbuild = $msbuilds[0].Definition
 }
 
-$cmdline = new-object System.Collections.ArrayList
-$cmdline.Add("xunit.msbuild") | out-null
-$cmdline.Add("/t:" + $target) | out-null
-$args | %{ $cmdline.Add($_) | out-null }
-
-& $msbuild $cmdline
+$allArgs = @("xunit.msbuild", "/t:$target", $args)
+& $msbuild $allArgs
