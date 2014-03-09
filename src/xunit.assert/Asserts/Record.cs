1c65d24fe97898370eb79c1c225eea3f76459bd3
Brad Wilson <dotnetguy@gmail.com>
2014-03-08 22:03:26 -0800

    Make Record a partial class.

--- src/xunit.assert/Asserts/Record.cs
+++ src/xunit.assert/Asserts/Record.cs
@@ -8,7 +8,7 @@ namespace Xunit
     /// Allows the user to record actions for a test.
     /// </summary>
     [SuppressMessage("Microsoft.Design", "CA1053:StaticHolderTypesShouldNotHaveConstructors", Justification = "This is not marked as static because we want people to be able to derive from it")]
-    public class Record
+    public partial class Record
     {
         /// <summary>
         /// Records any exception which is thrown by the given code.
