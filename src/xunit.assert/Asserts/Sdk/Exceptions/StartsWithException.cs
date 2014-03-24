bde01b9a690b0eea4d939d6682002f23b7d56c84
Erik Schierboom <erik_schierboom@hotmail.com>
2014-03-24 16:09:51 +0100

    Added Assert.StartsWith and Assert.EndsWith overloads. Part of #32

---
+++ src/xunit.assert/Asserts/Sdk/Exceptions/StartsWithException.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Xunit.Sdk
+{
+    /// <summary>
+    /// Exception thrown when a string does not start with the expected value.
+    /// </summary>
+    [SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors")]
+    public class StartsWithException : XunitException
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="StartsWithException"/> class.
+        /// </summary>
+        /// <param name="expected">The expected string value</param>
+        /// <param name="actual">The actual value</param>
+        public StartsWithException(string expected, string actual)
+            : base(String.Format(CultureInfo.CurrentCulture, "Assert.StartsWith() Failure:{2}Expected: {0}{2}Actual:   {1}", expected ?? "(null)", ShortenActual(expected, actual) ?? "(null)", Environment.NewLine)) { }
+        
+        static string ShortenActual(string expected, string actual)
+        {
+            if (expected == null || actual == null || actual.Length <= expected.Length)
+            {
+                return actual;
+            }
+
+            return actual.Substring(0, expected.Length) + "...";
+        }
+    }
+}
---
+++ test/test.xunit.assert/Asserts/Sdk/Exceptions/StartsWithExceptionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Xunit;
+
+public class StartsWithExceptionTests
+{
+    [Fact]
+    public void ActualStringNotLongerThanActualStringDoesNotTruncateActualString()
+    {
+        string expectedMessage =
+            "Assert.StartsWith() Failure:" + Environment.NewLine +
+            "Expected: WORLD" + Environment.NewLine +
+            "Actual:   Hello";
+
+        var ex = Record.Exception(() => Assert.StartsWith("WORLD", "Hello"));
+
+        Assert.Equal(expectedMessage, ex.Message);
+    }
+
+    [Fact]
+    public void ActualStringLongerThanActualStringTruncatesActualString()
+    {
+        string expectedMessage =
+            "Assert.StartsWith() Failure:" + Environment.NewLine +
+            "Expected: WORLD" + Environment.NewLine +
+            "Actual:   Hello...";
+
+        var ex = Record.Exception(() => Assert.StartsWith("WORLD", "Hello, world!"));
+
+        Assert.Equal(expectedMessage, ex.Message);
+    }
+
+    [Fact]
+    public void ActualStringIsNullAndExpectedIsNotShowsNullPlaceholderText()
+    {
+        string expectedMessage =
+            "Assert.StartsWith() Failure:" + Environment.NewLine +
+            "Expected: first test 1" + Environment.NewLine +
+            "Actual:   (null)";
+
+        var ex = Record.Exception(() => Assert.StartsWith("first test 1", null));
+
+        Assert.Equal(expectedMessage, ex.Message);
+    }
+
+    [Fact]
+    public void ExpectedStringIsNullAndActualIsNotShowsNullPlaceholderText()
+    {
+        string expectedMessage =
+            "Assert.StartsWith() Failure:" + Environment.NewLine +
+            "Expected: (null)" + Environment.NewLine +
+            "Actual:   first test 1";
+
+        var ex = Record.Exception(() => Assert.StartsWith(null, "first test 1"));
+
+        Assert.Equal(expectedMessage, ex.Message);
+    }
+}
