8219e01ac093f175d910df879d30dd5a5e28301d
Oren Novotny <oren@novotny.org>
2014-06-22 12:34:39 -0400

    Get utility libs compiling again

--- src/xunit.runner.visualstudio.testadapter/Visitors/VsDiscoveryVisitor.cs
+++ src/xunit.runner.visualstudio.testadapter/Visitors/VsDiscoveryVisitor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
