23824d56064a4446745f0fe289e5e659bc78f854
derkhalifa <derkhalifa@gmail.com>
2014-07-02 19:01:24 -0700

    Adding Tests to TestCaseFilterHelper

--- src/xunit.runner.visualstudio.testadapter/VsTestRunner.cs
+++ src/xunit.runner.visualstudio.testadapter/VsTestRunner.cs
@@ -157,20 +157,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                                                                     .ToList());
 
                                 var filterHelper = new TestCaseFilterHelper(knownTraitNames);
-                                ITestCaseFilterExpression filter = null;
-                                if (filterHelper.GetTestCaseFilterExpression(runContext, logger, stopwatch, assemblyFileName, out filter))
-                                {
-                                    if (filter != null)
-                                    {
-                                        grouping = new Grouping<string, TestCase>(grouping.Key, grouping.Where(testCase => filter.MatchTestCase(testCase, (p) => filterHelper.PropertyProvider(testCase, p))).ToList());
-                                    }
-                                }
-                                else
-                                {
-                                    // Error while filtering, ensure discovered test list is empty
-                                    grouping = new Grouping<string, TestCase>(assemblyFileName, new List<TestCase>());
-                                }
-
+                                grouping = filterHelper.GetFilteredTestList(grouping, runContext, logger, stopwatch, assemblyFileName);
                                 result.Add(grouping);
 
                                 if (settings.MessageDisplay != MessageDisplay.None)
@@ -340,109 +327,129 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 
             return @event;
         }
+    }
 
-        class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    public class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        readonly IEnumerable<TElement> elements;
+
+        public Grouping(TKey key, IEnumerable<TElement> elements)
         {
-            readonly IEnumerable<TElement> elements;
-
-            public Grouping(TKey key, IEnumerable<TElement> elements)
-            {
-                Key = key;
-                this.elements = elements;
-            }
-
-            public TKey Key { get; private set; }
-
-            public IEnumerator<TElement> GetEnumerator()
-            {
-                return elements.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return elements.GetEnumerator();
-            }
+            Key = key;
+            this.elements = elements;
         }
 
-        class TestCaseFilterHelper
+        public TKey Key { get; private set; }
+
+        public IEnumerator<TElement> GetEnumerator()
         {
-            private const string DisplayNameString = "DisplayName";
-            private const string FullyQualifiedNameString = "FullyQualifiedName";
-            private HashSet<string> knownTraits;
-            private List<string> supportedPropertyNames;
+            return elements.GetEnumerator();
+        }
 
-            public TestCaseFilterHelper(HashSet<string> knownTraits)
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }
+    }
+
+    public class TestCaseFilterHelper
+    {
+        private const string DisplayNameString = "DisplayName";
+        private const string FullyQualifiedNameString = "FullyQualifiedName";
+        private HashSet<string> knownTraits;
+        private List<string> supportedPropertyNames;
+
+        public TestCaseFilterHelper(HashSet<string> knownTraits)
+        {
+            this.knownTraits = knownTraits;
+            this.supportedPropertyNames = this.GetSupportedPropertyNames();
+        }
+
+        public Grouping<string, TestCase> GetFilteredTestList(Grouping<string, TestCase> grouping, IRunContext runContext, IMessageLogger logger, Stopwatch stopwatch, string assemblyFileName)
+        {
+            ITestCaseFilterExpression filter = null;
+            if (this.GetTestCaseFilterExpression(runContext, logger, stopwatch, assemblyFileName, out filter))
             {
-                this.knownTraits = knownTraits;
-                this.supportedPropertyNames = this.GetSupportedPropertyNames();
+                if (filter != null)
+                {
+                    return new Grouping<string, TestCase>(grouping.Key, grouping.Where(testCase => filter.MatchTestCase(testCase, (p) => this.PropertyProvider(testCase, p))).ToList());
+                }
+            }
+            else
+            {
+                // Error while filtering, ensure discovered test list is empty
+                return new Grouping<string, TestCase>(assemblyFileName, new List<TestCase>());
             }
 
-            private List<string> GetSupportedPropertyNames()
+            // No filter is specified return the original list
+            return grouping;
+        }
+
+        public object PropertyProvider(TestCase testCase, string name)
+        {
+            // Traits filtering
+            if (this.knownTraits.Contains(name))
             {
-                // Returns the set of well-known property names usually used with the Test Plugins (Used Test Traits + DisplayName + FullyQualifiedName)
-                if (this.supportedPropertyNames == null)
+                List<string> result = new List<string>();
+                foreach (Trait t in testCase.Traits)
                 {
-                    this.supportedPropertyNames = this.knownTraits.ToList();
-                    this.supportedPropertyNames.Add(DisplayNameString);
-                    this.supportedPropertyNames.Add(FullyQualifiedNameString);
+                    if (String.Equals(t.Name, name, StringComparison.OrdinalIgnoreCase))
+                        result.Add(t.Value);
                 }
-                return this.supportedPropertyNames;
+
+                if (result.Count > 0)
+                {
+                    return result.ToArray();
+                }
+            }
+            else
+            {
+                // Handle the displayName and fullyQualifierNames independently
+                if (String.Equals(name, FullyQualifiedNameString, StringComparison.OrdinalIgnoreCase))
+                    return testCase.FullyQualifiedName;
+                if (String.Equals(name, DisplayNameString, StringComparison.OrdinalIgnoreCase))
+                    return testCase.DisplayName;
             }
 
-            public bool GetTestCaseFilterExpression(IRunContext runContext, IMessageLogger logger, Stopwatch stopwatch, string assemblyFileName, out ITestCaseFilterExpression filter)
+            return null;
+        }
+
+        private bool GetTestCaseFilterExpression(IRunContext runContext, IMessageLogger logger, Stopwatch stopwatch, string assemblyFileName, out ITestCaseFilterExpression filter)
+        {
+            filter = null;
+            try
             {
-                filter = null;
-                try
+                // In Microsoft.VisualStudio.TestPlatform.ObjectModel V11 IRunContext provides a TestCaseFilter property
+                // GetTestCaseFilter only exists in V12+
+                MethodInfo getTestCaseFilterMethod = runContext.GetType().GetMethod("GetTestCaseFilter");
+                if (getTestCaseFilterMethod != null)
                 {
-                    // In Microsoft.VisualStudio.TestPlatform.ObjectModel V11 IRunContext provides a TestCaseFilter property
-                    // GetTestCaseFilter only exists in V12+
-                    MethodInfo getTestCaseFilterMethod = runContext.GetType().GetMethod("GetTestCaseFilter");
-                    if (getTestCaseFilterMethod != null)
-                    {
-                        filter = (ITestCaseFilterExpression)getTestCaseFilterMethod.Invoke(runContext, new object[] { this.supportedPropertyNames, null });
-                    }
-                    return true;
+                    filter = (ITestCaseFilterExpression)getTestCaseFilterMethod.Invoke(runContext, new object[] { this.supportedPropertyNames, null });
                 }
-                catch (TargetInvocationException e)
-                {
-                    var formatException = e.InnerException as TestPlatformFormatException;
-                    if (formatException != null)
-                    {
-                        logger.SendMessage(TestMessageLevel.Error,
-                            String.Format("[xUnit.net {0}] Exception discovering tests from {1}: Invalid filter string {2}", stopwatch.Elapsed, Path.GetFileName(assemblyFileName), formatException.FilterValue));
-                    }
-                }
-                return false;
+                return true;
             }
-
-            public object PropertyProvider(TestCase testCase, string name)
+            catch (TargetInvocationException e)
             {
-                // Traits filtering
-                if (this.knownTraits.Contains(name))
+                var formatException = e.InnerException as TestPlatformFormatException;
+                if (formatException != null)
                 {
-                    List<string> result = new List<string>();
-                    foreach (Trait t in testCase.Traits)
-                    {
-                        if (String.Equals(t.Name, name, StringComparison.OrdinalIgnoreCase))
-                            result.Add(t.Value);
-                    }
-
-                    if (result.Count > 0)
-                    {
-                        return result.ToArray();
-                    }
+                    logger.SendMessage(TestMessageLevel.Error,
+                        String.Format("[xUnit.net {0}] Exception discovering tests from {1}: Invalid filter string {2}", stopwatch.Elapsed, Path.GetFileName(assemblyFileName), formatException.FilterValue));
                 }
-                else
-                {
-                    // Handle the displayName and fullyQualifierNames independently
-                    if (String.Equals(name, FullyQualifiedNameString, StringComparison.OrdinalIgnoreCase))
-                        return testCase.FullyQualifiedName;
-                    if (String.Equals(name, DisplayNameString, StringComparison.OrdinalIgnoreCase))
-                        return testCase.DisplayName;
-                }
-
-                return null;
             }
+            return false;
+        }
+
+        private List<string> GetSupportedPropertyNames()
+        {
+            // Returns the set of well-known property names usually used with the Test Plugins (Used Test Traits + DisplayName + FullyQualifiedName)
+            if (this.supportedPropertyNames == null)
+            {
+                this.supportedPropertyNames = this.knownTraits.ToList();
+                this.supportedPropertyNames.Add(DisplayNameString);
+                this.supportedPropertyNames.Add(FullyQualifiedNameString);
+            }
+            return this.supportedPropertyNames;
         }
     }
 }
---
+++ test/test.xunit.runner.visualstudio.testadapter/TestCaseFilterHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xunit.Runner.VisualStudio.TestAdapter
+{
+    public class TestCaseFilterHelperTests
+    {
+        private HashSet<string> dummyKnownTraits = new HashSet<string>(new string[2] { "Platform", "Product" });
+
+        private Grouping<string, TestCase> GetDummyTestCases()
+        {
+            List<TestCase> testCaseList = new List<TestCase>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                testCaseList.Add(new TestCase("Test" + i.ToString(), new Uri(Constants.ExecutorUri), "DummyTestSource"));
+            }
+
+            return new Grouping<string, TestCase>("dummyTestAssembly", testCaseList);
+        }
+
+        [Fact]
+        public void TestCaseFilter_SingleMatch()
+        {
+            TestCaseFilterHelper filterHelper = new TestCaseFilterHelper(dummyKnownTraits);
+            Grouping<string, TestCase> dummyTestCaseList = GetDummyTestCases();
+            string dummyTestCaseDisplayNamefilterString = "Test4";
+            var context = Substitute.For<IRunContext>();
+            var logger = Substitute.For<IMessageLogger>();
+            var filterExpression = Substitute.For<ITestCaseFilterExpression>();
+
+            // The matching should return a single testcase
+            filterExpression.MatchTestCase(Arg.Any<TestCase>(), Arg.Any<Func<string, object>>()).Returns(x => ((TestCase)x[0]).FullyQualifiedName.Equals(dummyTestCaseDisplayNamefilterString));
+
+            context.GetTestCaseFilter(null, null).ReturnsForAnyArgs(filterExpression);
+
+            var result = filterHelper.GetFilteredTestList(dummyTestCaseList, context, logger, new Stopwatch(), "dummyTestAssembly");
+
+            Assert.Equal(1, result.Count());
+            Assert.Equal("Test4", result.First().FullyQualifiedName);
+        }
+
+        [Fact]
+        public void TestCaseFilter_NoFilterString()
+        {
+            TestCaseFilterHelper filterHelper = new TestCaseFilterHelper(dummyKnownTraits);
+            Grouping<string, TestCase> dummyTestCaseList = GetDummyTestCases();
+            var context = Substitute.For<IRunContext>();
+            var logger = Substitute.For<IMessageLogger>();
+            context.GetTestCaseFilter(null, null).ReturnsForAnyArgs((ITestCaseFilterExpression)null);
+            var result = filterHelper.GetFilteredTestList(dummyTestCaseList, context, logger, new Stopwatch(), "dummyTestAssembly");
+            // Make sure we run the whole set since there is not filtering string specified
+            Assert.Equal(dummyTestCaseList.Count(), result.Count());
+        }
+
+        [Fact]
+        public void TestCaseFilter_ErrorParsingFilterString()
+        {
+            TestCaseFilterHelper filterHelper = new TestCaseFilterHelper(dummyKnownTraits);
+            Grouping<string, TestCase> dummyTestCaseList = GetDummyTestCases();
+            var context = Substitute.For<IRunContext>();
+            var logger = Substitute.For<IMessageLogger>();
+            context.GetTestCaseFilter(null, null).ReturnsForAnyArgs(x => { throw new TestPlatformFormatException(); });
+            var result = filterHelper.GetFilteredTestList(dummyTestCaseList, context, logger, new Stopwatch(), "dummyTestAssembly");
+
+            // Make sure we don't run anything due to the filtering string parse error
+            Assert.Equal(0, result.Count());
+        }
+    }
+}
