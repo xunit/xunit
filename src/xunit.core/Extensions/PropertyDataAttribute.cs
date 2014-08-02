956131c56d87e0556c00be4cea02cf2815c44e0a
Brad Wilson <dotnetguy@gmail.com>
2014-08-02 14:02:56 -0700

    #150: Ease v1 -> v2 migration.

---
+++ src/xunit.core/Extensions/PropertyDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Xunit.Extensions
+{
+    /// <summary/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Please replace [PropertyData] with [MemberData]", error: true)]
+    public sealed class PropertyDataAttribute : Attribute
+    {
+        /// <summary/>
+        public PropertyDataAttribute(string propertyName) { }
+
+        /// <summary/>
+        public Type PropertyType { get; set; }
+    }
+}
