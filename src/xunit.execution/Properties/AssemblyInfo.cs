5ce02afc23a462208a64dc54a4b009c94d848d20
Oren Novotny <oren@novotny.org>
2014-08-02 22:43:28 -0400

    Add platform names to dll's to support troubleshooting

--- src/xunit.execution/Properties/AssemblyInfo.cs
+++ src/xunit.execution/Properties/AssemblyInfo.cs
@@ -1,5 +1,18 @@
 using System;
 using System.Reflection;
 
-[assembly: AssemblyTitle("xUnit.net")]
+#if ANDROID
+[assembly: AssemblyTitle("xUnit.net (MonoAndroid)")]
+#elif __IOS__
+[assembly: AssemblyTitle("xUnit.net (MonoTouch)")]
+#elif NO_APPDOMAIN
+[assembly: AssemblyTitle("xUnit.net (.NET 4.5 [No AppDomain])")]
+#elif NO_SERIALIZATION
+[assembly: AssemblyTitle("xUnit.net (.NET 4.5 [String Serialization])")]
+#elif WINDOWS_PHONE_APP
+[assembly: AssemblyTitle("xUnit.net (WPA81 + WIN81)")]
+#else
+[assembly: AssemblyTitle("xUnit.net (.NET 4.5)")]
+#endif
+
 [assembly: CLSCompliant(true)]
--- src/xunit.runner.utility/Properties/AssemblyInfo.cs
+++ src/xunit.runner.utility/Properties/AssemblyInfo.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyTitle("xUnit.net Runner Utility")]
+#if ANDROID
+[assembly: AssemblyTitle("xUnit.net Runner Utility (MonoAndroid)")]
+#elif __IOS__
+[assembly: AssemblyTitle("xUnit.net Runner Utility (MonoTouch)")]
+#elif NO_APPDOMAIN
+[assembly: AssemblyTitle("xUnit.net Runner Utility (.NET 4.5 [No AppDomain])")]
+#elif WINDOWS_PHONE_APP
+[assembly: AssemblyTitle("xUnit.net Runner Utility (WPA81 + WIN81)")]
+#else
+[assembly: AssemblyTitle("xUnit.net Runner Utility (.NET 3.5)")]
+#endif
+
 [assembly: CLSCompliant(true)]
