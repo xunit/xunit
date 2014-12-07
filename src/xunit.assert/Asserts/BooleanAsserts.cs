1e41e82234ac69a9468ec984c3e2b1aefa14439f
Brad Wilson <dotnetguy@gmail.com>
2014-12-07 11:53:37 -0800

    Need both bool and bool? Assert.True/False (F# won't do implicit conversion)

--- src/xunit.assert/Asserts/BooleanAsserts.cs
+++ src/xunit.assert/Asserts/BooleanAsserts.cs
@@ -9,9 +9,30 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be tested</param>
         /// <exception cref="FalseException">Thrown if the condition is not false</exception>
+        public static void False(bool condition)
+        {
+            False((bool?)condition, null);
+        }
+
+        /// <summary>
+        /// Verifies that the condition is false.
+        /// </summary>
+        /// <param name="condition">The condition to be tested</param>
+        /// <exception cref="FalseException">Thrown if the condition is not false</exception>
         public static void False(bool? condition)
         {
             False(condition, null);
+        }
+
+        /// <summary>
+        /// Verifies that the condition is false.
+        /// </summary>
+        /// <param name="condition">The condition to be tested</param>
+        /// <param name="userMessage">The message to show when the condition is not false</param>
+        /// <exception cref="FalseException">Thrown if the condition is not false</exception>
+        public static void False(bool condition, string userMessage)
+        {
+            False((bool?)condition, userMessage);
         }
 
         /// <summary>
@@ -31,9 +52,30 @@ namespace Xunit
         /// </summary>
         /// <param name="condition">The condition to be inspected</param>
         /// <exception cref="TrueException">Thrown when the condition is false</exception>
+        public static void True(bool condition)
+        {
+            True((bool?)condition, null);
+        }
+
+        /// <summary>
+        /// Verifies that an expression is true.
+        /// </summary>
+        /// <param name="condition">The condition to be inspected</param>
+        /// <exception cref="TrueException">Thrown when the condition is false</exception>
         public static void True(bool? condition)
         {
             True(condition, null);
+        }
+
+        /// <summary>
+        /// Verifies that an expression is true.
+        /// </summary>
+        /// <param name="condition">The condition to be inspected</param>
+        /// <param name="userMessage">The message to be shown when the condition is false</param>
+        /// <exception cref="TrueException">Thrown when the condition is false</exception>
+        public static void True(bool condition, string userMessage)
+        {
+            True((bool?)condition, userMessage);
         }
 
         /// <summary>
