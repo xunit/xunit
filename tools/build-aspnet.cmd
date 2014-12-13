cee5249be4315c55a40a410daeb2e41ad8570536
Brad Wilson <dotnetguy@gmail.com>
2014-12-13 15:29:29 -0800

    Compensate for the CLR team's inability to properly understand NuGet

--- tools/build-aspnet.cmd
+++ tools/build-aspnet.cmd
@@ -1,4 +1,7 @@
 @echo off
+rem Have to blow away the package cache because the CLR team can't get its shit together and publish sane packages
+rmdir /q /s %USERPROFILE%\.kpm\packages
+
 call tools\kvm install 1.0.0-beta1 -runtime CLR -x86
 call tools\kvm install 1.0.0-beta1 -runtime CoreCLR -x86
 call tools\kvm use 1.0.0-beta1 -runtime CLR -x86
