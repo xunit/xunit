90ac992c11daff1c4d7cc2a7f969388c9a018160
Brad Wilson <dotnetguy@gmail.com>
2015-01-07 13:48:21 -0800

    Fixes #252: Unused memory stream

--- src/xunit.execution/Sdk/Frameworks/TheoryDiscoverer.cs
+++ src/xunit.execution/Sdk/Frameworks/TheoryDiscoverer.cs
@@ -28,38 +28,35 @@ namespace Xunit.Sdk
             {
                 try
                 {
-                    using (var memoryStream = new MemoryStream())
+                    var results = new List<XunitTestCase>();
+
+                    foreach (var dataAttribute in dataAttributes)
                     {
-                        var results = new List<XunitTestCase>();
+                        var discovererAttribute = dataAttribute.GetCustomAttributes(typeof(DataDiscovererAttribute)).First();
+                        var discoverer = ExtensibilityPointFactory.GetDataDiscoverer(discovererAttribute);
+                        if (!discoverer.SupportsDiscoveryEnumeration(dataAttribute, testMethod.Method))
+                            return new XunitTestCase[] { new XunitTheoryTestCase(defaultMethodDisplay, testMethod) };
 
-                        foreach (var dataAttribute in dataAttributes)
+                        // GetData may return null, but that's okay; we'll let the NullRef happen and then catch it
+                        // down below so that we get the composite test case.
+                        foreach (var dataRow in discoverer.GetData(dataAttribute, testMethod.Method))
                         {
-                            var discovererAttribute = dataAttribute.GetCustomAttributes(typeof(DataDiscovererAttribute)).First();
-                            var discoverer = ExtensibilityPointFactory.GetDataDiscoverer(discovererAttribute);
-                            if (!discoverer.SupportsDiscoveryEnumeration(dataAttribute, testMethod.Method))
-                                return new XunitTestCase[] { new XunitTheoryTestCase(defaultMethodDisplay, testMethod) };
-
-                            // GetData may return null, but that's okay; we'll let the NullRef happen and then catch it
-                            // down below so that we get the composite test case.
-                            foreach (var dataRow in discoverer.GetData(dataAttribute, testMethod.Method))
-                            {
-                                // Attempt to serialize the test case, since we need a way to uniquely identify a test
-                                // and serialization is the best way to do that. If it's not serializable, this will
-                                // throw and we will fall back to a single theory test case that gets its data
-                                // at runtime.
-                                var testCase = new XunitTestCase(defaultMethodDisplay, testMethod, dataRow);
-                                SerializationHelper.Serialize(testCase);
-                                results.Add(testCase);
-                            }
+                            // Attempt to serialize the test case, since we need a way to uniquely identify a test
+                            // and serialization is the best way to do that. If it's not serializable, this will
+                            // throw and we will fall back to a single theory test case that gets its data
+                            // at runtime.
+                            var testCase = new XunitTestCase(defaultMethodDisplay, testMethod, dataRow);
+                            SerializationHelper.Serialize(testCase);
+                            results.Add(testCase);
                         }
-
-                        // REVIEW: Could we re-write LambdaTestCase to just be for exceptions?
-                        if (results.Count == 0)
-                            results.Add(new LambdaTestCase(defaultMethodDisplay, testMethod,
-                                                           () => { throw new InvalidOperationException(String.Format("No data found for {0}.{1}", testMethod.TestClass.Class.Name, testMethod.Method.Name)); }));
-
-                        return results;
                     }
+
+                    // REVIEW: Could we re-write LambdaTestCase to just be for exceptions?
+                    if (results.Count == 0)
+                        results.Add(new LambdaTestCase(defaultMethodDisplay, testMethod,
+                                                       () => { throw new InvalidOperationException(String.Format("No data found for {0}.{1}", testMethod.TestClass.Class.Name, testMethod.Method.Name)); }));
+
+                    return results;
                 }
                 catch { }
             }
