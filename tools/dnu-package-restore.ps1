529a754d8a3ea76859335146939e077d8faf0e42
Brad Wilson <dotnetguy@gmail.com>
2015-04-29 16:26:19 -0700

    Add global.json

--- tools/dnu-package-restore.ps1
+++ tools/dnu-package-restore.ps1
@@ -1,7 +1,9 @@
 $toolsPath = split-path $MyInvocation.MyCommand.Definition
-$solutionPath = [System.IO.Path]::GetFullPath($(join-path $toolsPath ".."))
 $dnvm = join-path $toolsPath "dnvm.ps1"
+$solutionPath = [System.IO.Path]::GetFullPath($(join-path $toolsPath ".."))
+$globalJson = join-path $solutionPath "global.json"
+$version = (ConvertFrom-JSON ([System.IO.File]::ReadAllText($globalJson))).sdk.version
 
-& $dnvm install 1.0.0-beta4
-& $dnvm use 1.0.0-beta4
+& $dnvm install $version
+& $dnvm use $version
 & dnu restore $solutionPath
