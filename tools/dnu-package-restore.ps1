6c439e71c4bd404a9a59d22fd6f75e1fa8f0c431
Brad Wilson <dotnetguy@gmail.com>
2015-04-29 17:07:14 -0700

    Call variable $dnxVersion instead of $version

--- tools/dnu-package-restore.ps1
+++ tools/dnu-package-restore.ps1
@@ -2,8 +2,8 @@ $toolsPath = split-path $MyInvocation.MyCommand.Definition
 $dnvm = join-path $toolsPath "dnvm.ps1"
 $solutionPath = [System.IO.Path]::GetFullPath($(join-path $toolsPath ".."))
 $globalJson = join-path $solutionPath "global.json"
-$version = (ConvertFrom-JSON ([System.IO.File]::ReadAllText($globalJson))).sdk.version
+$dnxVersion = (ConvertFrom-JSON ([System.IO.File]::ReadAllText($globalJson))).sdk.version
 
-& $dnvm install $version
-& $dnvm use $version
+& $dnvm install $dnxVersion
+& $dnvm use $dnxVersion
 & dnu restore $solutionPath
