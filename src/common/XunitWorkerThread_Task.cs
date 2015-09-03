30f2238f04a0d1583d7fb57d618e079f8b915543
Brad Wilson <dotnetguy@gmail.com>
2015-09-03 10:23:27 -0700

    Need a proper implementation of XunitWorkerThread_Task.Join

--- src/common/XunitWorkerThread_Task.cs
+++ src/common/XunitWorkerThread_Task.cs
@@ -6,21 +6,38 @@ namespace Xunit.Sdk
 {
     class XunitWorkerThread
     {
+        readonly ManualResetEvent finished = new ManualResetEvent(false);
         static readonly TaskFactory taskFactory = new TaskFactory();
 
         public XunitWorkerThread(Action threadProc)
         {
-            QueueUserWorkItem(threadProc);
+            QueueUserWorkItem(threadProc, finished);
         }
 
-        public void Join() { }
-
-        public static void QueueUserWorkItem(Action backgroundTask)
+        public void Join()
         {
-            taskFactory.StartNew(backgroundTask,
+            finished.WaitOne();
+        }
+
+        public static void QueueUserWorkItem(Action backgroundTask, ManualResetEvent finished = null)
+        {
+            taskFactory.StartNew(_ =>
+                                 {
+                                     var state = (State)_;
+                                     state.BackgroundTask();
+                                     if (state.Finished != null)
+                                         state.Finished.Set();
+                                 },
+                                 new State { BackgroundTask = backgroundTask, Finished = finished },
                                  CancellationToken.None,
                                  TaskCreationOptions.LongRunning,
                                  TaskScheduler.Default);
+        }
+
+        class State
+        {
+            public Action BackgroundTask;
+            public ManualResetEvent Finished;
         }
     }
 }
