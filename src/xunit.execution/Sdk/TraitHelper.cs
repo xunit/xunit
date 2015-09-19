47fdc2669ae6aa28f6d642e202840193dfc7dbd7
Brad Wilson <dotnetguy@gmail.com>
2015-09-19 12:03:08 -0700

    Fixes #588: Add TraitHelper to programmatically get traits

---
+++ src/xunit.execution/Sdk/TraitHelper.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Xunit.Sdk
+{
+    /// <summary>
+    /// A helper class to retrieve the traits from a method.
+    /// </summary>
+    public static class TraitHelper
+    {
+        /// <summary>
+        /// Get the traits from a method.
+        /// </summary>
+        /// <param name="method">The method to get the traits for.</param>
+        /// <returns>A list of traits that are defined on the method.</returns>
+        public static IReadOnlyList<KeyValuePair<string, string>> GetTraits(MethodInfo method)
+        {
+            var messageSink = new NullMessageSink();
+            var result = new List<KeyValuePair<string, string>>();
+
+            foreach (var traitAttributeData in method.CustomAttributes)
+            {
+                var traitAttributeType = traitAttributeData.AttributeType;
+                if (!typeof(ITraitAttribute).GetTypeInfo().IsAssignableFrom(traitAttributeType.GetTypeInfo()))
+                    continue;
+
+                var discovererAttributeData = traitAttributeType.GetTypeInfo().CustomAttributes.FirstOrDefault(cad => cad.AttributeType == typeof(TraitDiscovererAttribute));
+                if (discovererAttributeData == null)
+                    continue;
+
+                var discoverer = ExtensibilityPointFactory.GetTraitDiscoverer(messageSink, Reflector.Wrap(discovererAttributeData));
+                if (discoverer == null)
+                    continue;
+
+                var traits = discoverer.GetTraits(Reflector.Wrap(traitAttributeData));
+                if (traits != null)
+                    result.AddRange(traits);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/test.xunit.execution/Common/TraitHelperTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class TraitHelperTests
+{
+    [Fact]
+    public void MethodWithNoTraits_ReturnsEmptyList()
+    {
+        var method = typeof(ClassUnderTest).GetMethod("NoTraits");
+
+        var traits = TraitHelper.GetTraits(method);
+
+        Assert.Empty(traits);
+    }
+
+    [Fact]
+    public void MethodWithTraitAttribute_ReturnsTrait()
+    {
+        var method = typeof(ClassUnderTest).GetMethod("Trait");
+
+        var traits = TraitHelper.GetTraits(method);
+
+        Assert.Collection(traits.Select(kvp => $"{kvp.Key} = {kvp.Value}").OrderBy(_ => _, StringComparer.OrdinalIgnoreCase),
+            value => Assert.Equal("foo = bar", value)
+        );
+    }
+
+    [Fact]
+    public void MethodWithCustomTraitAttribute_ReturnsTraitValues()
+    {
+        var method = typeof(ClassUnderTest).GetMethod("CustomTrait");
+
+        var traits = TraitHelper.GetTraits(method);
+
+        Assert.Collection(traits.Select(kvp => $"{kvp.Key} = {kvp.Value}").OrderBy(_ => _, StringComparer.OrdinalIgnoreCase),
+            value => Assert.Equal("Baz = 2112", value),
+            value => Assert.Equal("Foo = Biff", value)
+        );
+    }
+
+    [Fact]
+    public void CombinesMultipleTraitSources()
+    {
+        var method = typeof(ClassUnderTest).GetMethod("MultipleTraits");
+
+        var traits = TraitHelper.GetTraits(method);
+
+        Assert.Collection(traits.Select(kvp => $"{kvp.Key} = {kvp.Value}").OrderBy(_ => _, StringComparer.OrdinalIgnoreCase),
+            value => Assert.Equal("Baz = 2112", value),
+            value => Assert.Equal("foo = bar", value),
+            value => Assert.Equal("Foo = Biff", value),
+            value => Assert.Equal("Hello = World", value)
+        );
+    }
+
+    class ClassUnderTest
+    {
+        [Obsolete]    // Irrelevant attribute
+        public void NoTraits() { }
+
+        [Trait("foo", "bar")]
+        public void Trait() { }
+
+        [CustomTrait]
+        public void CustomTrait() { }
+
+        [Trait("foo", "bar")]
+        [Trait("Hello", "World")]
+        [CustomTrait]
+        public void MultipleTraits() { }
+    }
+
+    [TraitDiscoverer("TraitHelperTests+CustomTraitDiscoverer", "test.xunit.execution")]
+    class CustomTraitAttribute : Attribute, ITraitAttribute { }
+
+    class CustomTraitDiscoverer : ITraitDiscoverer
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            yield return new KeyValuePair<string, string>("Foo", "Biff");
+            yield return new KeyValuePair<string, string>("Baz", "2112");
+        }
+    }
+}
