9d3a468105a68c70b80c0256ce3735270efe3d71
Brad Wilson <dotnetguy@gmail.com>
2015-11-14 15:37:33 -0800

    Fix for #660: Can't run tests from UNC path

--- src/common/AssemblyExtensions.cs
+++ src/common/AssemblyExtensions.cs
@@ -8,18 +8,41 @@ static class AssemblyExtensions
 {
     public static string GetLocalCodeBase(this Assembly assembly)
     {
-        var codeBase = assembly.CodeBase;
+        return GetLocalCodeBase(assembly?.CodeBase, Path.DirectorySeparatorChar);
+    }
+
+    public static string GetLocalCodeBase(string codeBase, char directorySeparator)
+    {
         if (codeBase == null)
             return null;
 
-        if (!codeBase.StartsWith("file:///", StringComparison.Ordinal))
-            throw new ArgumentException($"Code base {codeBase} in wrong format; must start with file:///", nameof(assembly));
+        if (!codeBase.StartsWith("file://", StringComparison.Ordinal))
+            throw new ArgumentException($"Codebase '{codeBase}' is unsupported; must start with 'file://'.", nameof(codeBase));
 
-        codeBase = codeBase.Substring(8);
-        if (Path.DirectorySeparatorChar == '/')
-            return "/" + codeBase;
+        // "file:///path" is a local path; "file://machine/path" is a UNC
+        var localFile = codeBase.Length > 7 && codeBase[7] == '/';
 
-        return codeBase.Replace('/', Path.DirectorySeparatorChar);
+        // POSIX-style directories
+        if (directorySeparator == '/')
+        {
+            if (localFile)
+                return codeBase.Substring(7);
+
+            throw new ArgumentException($"UNC-style codebase '{codeBase}' is not supported on POSIX-style file systems.", nameof(codeBase));
+        }
+
+        // Windows-style directories
+        if (directorySeparator == '\\')
+        {
+            codeBase = codeBase.Replace('/', '\\');
+
+            if (localFile)
+                return codeBase.Substring(8);
+
+            return codeBase.Substring(5);
+        }
+
+        throw new ArgumentException($"Unknown directory separator '{directorySeparator}'; must be one of '/' or '\\'.", nameof(directorySeparator));
     }
 }
 
