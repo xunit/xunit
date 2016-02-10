5cef1714aa9a786e3f9d662f58191c6855f2bd52
Matthias Auswöger <matthias.auswoeger@gmx.net>
2016-02-10 19:32:39 +0100

    Adding a test for the type cast issue.

--- test/test.xunit.execution/SerializationTests.cs
+++ test/test.xunit.execution/SerializationTests.cs
@@ -119,4 +119,33 @@ public class SerializationTests
         [MemberData("Data")]
         public void Test(object x) { }
     }
+
+    public class ClassWithTheoryDataTypeDiffrentToArgumentType
+    {
+        public class A
+        {
+        }
+
+        public class B
+        {
+            public static explicit operator A(B value)
+            {
+                return new A();
+            }
+
+            public static explicit operator B(A value)
+            {
+                return new B();
+            }
+        }
+
+        public static IEnumerable<object[]> Data = new[] { new object[] { new B() }, new object[] { new B() } };
+
+        [Theory]
+        [MemberData("Data")]
+        public void Test(A x)
+        {
+            Console.WriteLine("Test");
+        }
+    }
 }
