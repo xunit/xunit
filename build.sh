2f3be8c3b8a43abe93302df44dcefbc259cfcc91
Brad Wilson <dotnetguy@gmail.com>
2016-10-01 11:57:38 -0700

    Rewrite build.sh

--- build.sh
+++ build.sh
@@ -1,63 +1,66 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v mono)" ]; then
-	echo >&2 "Could not find 'mono' on the path. Please install Mono."
-	exit 1
+tmpfile=$(mktemp /tmp/xunit-build.XXXXXX)
+exec 3>"${tmpfile}"
+rm "${tmpfile}"
+
+if test -t 1; then
+	if [ -n `which tput` ]; then
+        COLOR_RED="$(tput setaf 1)"
+        COLOR_WHITE="$(tput setaf 7)"
+        COLOR_BOLD="$(tput bold)"
+        COLOR_RESET="$(tput sgr0)"
+    fi
 fi
 
-if ! [ -x "$(command -v dotnet)" ]; then
-	echo >&2 "Could not find 'dotnet' on the path. Please install .NET CLI."
+build_step() {
+	local MSG="$1"
+	echo -e "${COLOR_WHITE}${COLOR_BOLD}==> ${MSG}${COLOR_RESET}"
+}
+
+fatal() {
+	local MSG="$1"
+	echo -e "${COLOR_RED}${COLOR_BOLD}Error:${COLOR_RESET} ${MSG}"
+	echo
+	cat "${tmpfile}"
 	exit 1
-fi
+}
 
-echo ""
-echo "Restoring packages..."
-echo ""
+require() {
+	local BINARY="$1"
+	local NAME="$2"
+	[[ -n `which ${BINARY}` ]] || fatal "Could not find '${BINARY}' on the path. Please install ${NAME}."
+}
 
-dotnet restore
+require mono 'Mono'
+require dotnet '.NET CLI'
 
-if [ $? -ne 0 ]; then
-	echo >&2 "Package restore has failed."
-	exit 1
-fi
+build_step "Restoring NuGet packages"
 
-echo ""
-echo "Building..."
-echo ""
+	dotnet restore >"${tmpfile}" 2>&1 || fatal "Package restore has failed."
 
-dotnet build -c Release \
-	src/xunit.assert \
-	src/xunit.core \
-	src/xunit.execution \
-	src/xunit.runner.utility \
-	src/xunit.runner.reporters \
-	src/xunit.console \
-	test/test.xunit.assert \
-	test/test.xunit.execution \
-	test/test.xunit.runner.utility \
-	test/test.xunit.runner.reporters \
-	test/test.xunit.console
+build_step "Compiling binaries"
 
-if [ $? -ne 0 ]; then
-	echo >&2 ""
-	echo >&2 "The build has failed."
-	exit 1
-fi
+	dotnet build -c Release \
+		src/xunit.assert \
+		src/xunit.core \
+		src/xunit.execution \
+		src/xunit.runner.utility \
+		src/xunit.runner.reporters \
+		src/xunit.console \
+		test/test.xunit.assert \
+		test/test.xunit.execution \
+		test/test.xunit.runner.utility \
+		test/test.xunit.runner.reporters \
+		test/test.xunit.console >"${tmpfile}" 2>&1 \
+			|| fatal "The build has failed."
 
-#echo ""
-#echo "Running unit tests..."
-#echo ""
+build_step "Running unit tests\n"
 
-#mono src/xunit.console/bin/Release/net45/*/xunit.console.exe \
-#	test/test.xunit.assert/bin/Release/net45/test.xunit.assert.dll \
-#	test/test.xunit.execution/bin/Release/net45/test.xunit.execution.dll \
-#	test/test.xunit.runner.utility/bin/Release/net45/test.xunit.runner.utility.dll \
-#	test/test.xunit.runner.reporters/bin/Release/net45/test.xunit.runner.reporters.dll \
-#	test/test.xunit.console/bin/Release/net45/test.xunit.console.dll \
-#	-parallel all
-
-#if [ $? -ne 0 ]; then
-#	echo >&2 ""
-#	echo >&2 "Unit tests have failed."
-#	exit 1
-#fi
+	# TODO: execution & runner.utility tests fail on Mono
+	mono src/xunit.console/bin/Release/net45/**/xunit.console.exe \
+		test/test.xunit.assert/bin/Release/net45/test.xunit.assert.dll \
+		test/test.xunit.console/bin/Release/net45/test.xunit.console.dll \
+		test/test.xunit.runner.reporters/bin/Release/net45/test.xunit.runner.reporters.dll \
+		-noappdomain -parallel none \
+			|| fatal "Unit tests have failed."
