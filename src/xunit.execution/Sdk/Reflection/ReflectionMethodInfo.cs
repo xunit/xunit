0d83a59ce998db8f74f2eeee68de7b5d124e1cf3
Hugh Bellamy <hughbellars@gmail.com>
2016-10-16 23:42:03 +0100

    Improve perf and allocations of GetParameters (#966)
    Don't use linq
    Cache the result

--- src/xunit.execution/Sdk/Reflection/ReflectionMethodInfo.cs
+++ src/xunit.execution/Sdk/Reflection/ReflectionMethodInfo.cs
@@ -173,12 +173,23 @@ namespace Xunit.Sdk
             return MethodInfo.ToString();
         }
 
+        private IEnumerable<IParameterInfo> _cachedParameters = null;
+
         /// <inheritdoc/>
         public IEnumerable<IParameterInfo> GetParameters()
         {
-            return MethodInfo.GetParameters()
-                             .Select(p => Reflector.Wrap(p))
-                             .ToArray();
+            if (_cachedParameters == null)
+            {
+                ParameterInfo[] parameters = MethodInfo.GetParameters();
+
+                IParameterInfo[] iParameters = new IParameterInfo[parameters.Length];
+                for (int i = 0; i < iParameters.Length; i++)
+                {
+                    iParameters[i] = Reflector.Wrap(parameters[i]);
+                }
+                _cachedParameters = iParameters;
+            }
+            return _cachedParameters;
         }
 
         class GenericTypeComparer : IEqualityComparer
