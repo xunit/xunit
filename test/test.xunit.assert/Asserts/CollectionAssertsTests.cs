6e665dff0dc9d00fcf975ad5628d5d63e39d09bd
Hugh Bellamy <hughbellars@gmail.com>
2016-11-07 14:32:55 +0000

    Add tests for included collection in Assert.Collection failure

--- test/test.xunit.assert/Asserts/CollectionAssertsTests.cs
+++ test/test.xunit.assert/Asserts/CollectionAssertsTests.cs
@@ -77,6 +77,7 @@ public class CollectionAssertsTests
             Assert.Equal(1, collEx.ExpectedCount);
             Assert.Equal(0, collEx.ActualCount);
             Assert.Equal("Assert.Collection() Failure" + Environment.NewLine +
+                         "Collection: []" + Environment.NewLine +
                          "Expected item count: 1" + Environment.NewLine +
                          "Actual item count:   0", collEx.Message);
             Assert.Null(collEx.InnerException);
@@ -108,6 +109,7 @@ public class CollectionAssertsTests
             var collEx = Assert.IsType<CollectionException>(ex);
             Assert.Equal(1, collEx.IndexFailurePoint);
             Assert.Equal("Assert.Collection() Failure" + Environment.NewLine +
+                         "Collection: [42, 2112]" + Environment.NewLine +
                          "Error during comparison of item at index 1" + Environment.NewLine +
                          "Inner exception: Assert.Equal() Failure" + Environment.NewLine +
                          "        Expected: 2113" + Environment.NewLine +
