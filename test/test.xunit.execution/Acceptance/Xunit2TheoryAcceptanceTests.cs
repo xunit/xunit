e3ac35d9f14b371f181eede186fd4956a7697f83
Hugh Bellamy <hughbellars@gmail.com>
2017-02-12 12:00:23 +0700

    Improve error message if ClassData doesn't implement IEnumerable<object[]>

--- test/test.xunit.execution/Acceptance/Xunit2TheoryAcceptanceTests.cs
+++ test/test.xunit.execution/Acceptance/Xunit2TheoryAcceptanceTests.cs
@@ -493,9 +493,9 @@ public class Xunit2TheoryAcceptanceTests
             var testMessages = Run<ITestResultMessage>(typeof(ClassUnderTest));
 
             var passing = Assert.Single(testMessages.OfType<ITestPassed>());
-            Assert.Equal($"Xunit2TheoryAcceptanceTests+ClassDataTests+ClassUnderTest.TestViaInlineData(x: 42, y: {21.12}, z: \"Hello, world!\")", passing.Test.DisplayName);
+            Assert.Equal($"Xunit2TheoryAcceptanceTests+ClassDataTests+ClassUnderTest.TestViaClassData(x: 42, y: {21.12}, z: \"Hello, world!\")", passing.Test.DisplayName);
             var failed = Assert.Single(testMessages.OfType<ITestFailed>());
-            Assert.Equal("Xunit2TheoryAcceptanceTests+ClassDataTests+ClassUnderTest.TestViaInlineData(x: 0, y: 0, z: null)", failed.Test.DisplayName);
+            Assert.Equal("Xunit2TheoryAcceptanceTests+ClassDataTests+ClassUnderTest.TestViaClassData(x: 0, y: 0, z: null)", failed.Test.DisplayName);
             Assert.Empty(testMessages.OfType<ITestSkipped>());
         }
 
@@ -503,7 +503,7 @@ public class Xunit2TheoryAcceptanceTests
         {
             [Theory]
             [ClassData(typeof(ClassDataSource))]
-            public void TestViaInlineData(int x, double y, string z)
+            public void TestViaClassData(int x, double y, string z)
             {
                 Assert.NotNull(z);
             }
@@ -517,10 +517,25 @@ public class Xunit2TheoryAcceptanceTests
                 yield return new object[] { 0, 0.0, null };
             }
 
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Fact]
+        public void NoDefaultConstructor_Fails()
+        {
+            var testMessages = Run<ITestResultMessage>(typeof(ClassNotImplementingIEnumerable));
+
+            var failed = Assert.Single(testMessages.Cast<ITestFailed>());
+            Assert.Equal("Xunit2TheoryAcceptanceTests+ClassDataTests+ClassNotImplementingIEnumerable.TestMethod", failed.Test.DisplayName);
+            Assert.Equal("System.ArgumentException", failed.ExceptionTypes.Single());
+            Assert.Equal("Xunit2TheoryAcceptanceTests+ClassDataTests+ClassNotImplementingIEnumerable must implement IEnumerable<object[]> to be used as ClassData for the test method named 'TestMethod' on Xunit2TheoryAcceptanceTests+ClassDataTests+ClassNotImplementingIEnumerable", failed.Messages.Single());
+        }
+
+        class ClassNotImplementingIEnumerable
+        {
+            [Theory]
+            [ClassData(typeof(ClassNotImplementingIEnumerable))]
+            public void TestMethod() { }
         }
     }
 
