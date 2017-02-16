efb84878af93d521c7805f95fe8c5a68e913c27c
Brad Wilson <dotnetguy@gmail.com>
2017-02-16 14:21:52 -0800

    Add netstandard1.5 version of xunit.runner.reporters

--- src/xunit.runner.reporters/Properties/AssemblyInfo.cs
+++ src/xunit.runner.reporters/Properties/AssemblyInfo.cs
@@ -4,6 +4,8 @@
 [assembly: AssemblyTitle("xUnit.net Runner Reporters (.NET 4.5.2)")]
 #elif NETSTANDARD1_1
 [assembly: AssemblyTitle("xUnit.net Runner Reporters (.NET Standard 1.1)")]
+#elif NETSTANDARD1_5
+[assembly: AssemblyTitle("xUnit.net Runner Reporters (.NET Standard 1.5)")]
 #else
 #error Unknown target platform
 #endif
