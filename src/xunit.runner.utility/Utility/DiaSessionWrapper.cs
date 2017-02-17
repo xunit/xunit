03db1249789c1285114bea1adf3b039ac71c9688
Shyam-Gupta <Shyam-Gupta@users.noreply.github.com>
2017-02-17 14:54:44 -0800

    Removed extra appdomain getting created for SourceInformation

--- src/xunit.runner.utility/Utility/DiaSessionWrapper.cs
+++ src/xunit.runner.utility/Utility/DiaSessionWrapper.cs
@@ -8,31 +8,21 @@ namespace Xunit
     // discover when a test is an async test (since that requires special handling by DIA).
     class DiaSessionWrapper : IDisposable
     {
-        readonly AppDomainManager_AppDomain appDomainManager;
-        readonly DiaSessionWrapperHelper helper;
         readonly DiaSession session;
 
         public DiaSessionWrapper(string assemblyFilename)
         {
             session = new DiaSession(assemblyFilename);
-
-            var assemblyFileName = typeof(DiaSessionWrapperHelper).Assembly.GetLocalCodeBase();
-
-            appDomainManager = new AppDomainManager_AppDomain(assemblyFileName, null, true, null);
-            helper = appDomainManager.CreateObject<DiaSessionWrapperHelper>(typeof(DiaSessionWrapperHelper).Assembly.GetName(), typeof(DiaSessionWrapperHelper).FullName, assemblyFilename);
         }
 
         public DiaNavigationData GetNavigationData(string typeName, string methodName)
         {
-            var owningAssemblyFilename = session.AssemblyFileName;
-            helper.Normalize(ref typeName, ref methodName, ref owningAssemblyFilename);
-            return session.GetNavigationData(typeName, methodName, owningAssemblyFilename);
+            return session.GetNavigationData(typeName, methodName, session.AssemblyFileName);
         }
 
         public void Dispose()
         {
             session.Dispose();
-            appDomainManager.Dispose();
         }
     }
 }
