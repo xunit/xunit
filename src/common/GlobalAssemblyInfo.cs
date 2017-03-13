9740b5b94cfa382aa2aeb8ac255bbd925711cd8c
Brad Wilson <dotnetguy@gmail.com>
2017-03-12 20:22:26 -0700

    Use embedded symbols instead of symbol packages

--- src/common/GlobalAssemblyInfo.cs
+++ src/common/GlobalAssemblyInfo.cs
@@ -4,3 +4,5 @@
 [assembly: AssemblyProduct("xUnit.net Testing Framework")]
 [assembly: AssemblyCopyright("Copyright (C) .NET Foundation")]
 [assembly: AssemblyVersion("99.99.99.0")]
+[assembly: AssemblyFileVersion("99.99.99.0")]
+[assembly: AssemblyInformationalVersionAttribute("99.99.99-dev")]
