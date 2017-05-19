2febd0f383a6c847a2a043f078b8c695760985d5
Santiago Fernandez <safern@microsoft.com>
2017-05-18 17:42:57 -0700

    Add comments to serializable attribute

--- src/common/SerializableAttribute.cs
+++ src/common/SerializableAttribute.cs
@@ -1,9 +1,15 @@
 
 namespace System {
 #if !NET35 && !NET452
+    /// <summary>
+    /// Attribute to mark a class as serializable
+    /// </summary> 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface, AllowMultiple = true)]
     public sealed class SerializableAttribute : Attribute
     {
+        /// <summary>
+        /// Default constructor that creates a new instance of the <see cref="SerializableAttribute"/> type.
+        /// </summary> 
         public SerializableAttribute() {
         }
     }
