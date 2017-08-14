3a5eb52e26dabc5948522620c21981301fed5627
Brad Wilson <dotnetguy@gmail.com>
2017-08-14 10:19:02 -0700

    Make desktop and UAP VS runners use the same URI

--- src/xunit.runner.visualstudio/Constants.cs
+++ src/xunit.runner.visualstudio/Constants.cs
@@ -2,12 +2,10 @@
 {
     public static class Constants
     {
-#if NET452
-        public const string ExecutorUri = "executor://xunit/VsTestRunner2/desktop";
+#if NET452 || WINDOWS_UAP
+        public const string ExecutorUri = "executor://xunit/VsTestRunner2/desktop_and_uap";
 #elif NETCOREAPP1_0
         public const string ExecutorUri = "executor://xunit/VsTestRunner2/netcoreapp";
-#elif WINDOWS_UAP
-        public const string ExecutorUri = "executor://xunit/VsTestRunner2/uap";
 #else
 #error Unknown target platform
 #endif
