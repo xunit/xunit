7d7da9242e4a18644d7531e5aa044df255c51d40
Brad Wilson <dotnetguy@gmail.com>
2017-08-15 00:49:14 -0700

    Wrong #if in GlobalTestAssemblyInfo.cs

--- test/GlobalTestAssemblyInfo.cs
+++ test/GlobalTestAssemblyInfo.cs
@@ -4,6 +4,6 @@ using Xunit;
 [assembly: AssemblyVersion("99.99.99.0")]
 [assembly: AssemblyTrait("Assembly", "Trait")]
 
-#if NET45
+#if NET452
 [assembly: TestDriven.Framework.CustomTestRunner(typeof(Xunit.Runner.TdNet.TdNetRunner))]
 #endif
