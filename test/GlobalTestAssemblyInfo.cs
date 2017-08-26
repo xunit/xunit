bbcb06af9a836041407729989f467aa4cf638ad4
Brad Wilson <dotnetguy@gmail.com>
2017-08-26 12:55:08 -0700

    CSPROJ cleanliness

--- test/GlobalTestAssemblyInfo.cs
+++ test/GlobalTestAssemblyInfo.cs
@@ -2,7 +2,10 @@ using System.Reflection;
 using Xunit;
 
 [assembly: AssemblyVersion("99.99.99.0")]
+
+#if !NET40
 [assembly: AssemblyTrait("Assembly", "Trait")]
+#endif
 
 #if NET452
 [assembly: TestDriven.Framework.CustomTestRunner(typeof(Xunit.Runner.TdNet.TdNetRunner))]
