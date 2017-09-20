6b58a2a983de6c7d590d2533341afb25807a0323
Brad Wilson <dotnetguy@gmail.com>
2017-09-19 23:46:53 -0700

    Adding PerfTest.ps1 for simple comparisons

---
+++ PerfTest.ps1
@@ -0,0 +1,11 @@
+Write-Host -Foreground Yellow -NoNewLine "vstest.console.exe (net452): "
+Write-Host (Measure-Command { & "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" /TestAdapterPath:src\xunit.runner.visualstudio\bin\Debug\net452 /ListTests /Settings:test\test.harness\Settings.runsettings test\test.harness\bin\Debug\net452\test.harness.dll }).TotalMilliseconds
+
+Write-Host -Foreground Yellow -NoNewLine "xunit.console.exe (net452):  "
+Write-Host (Measure-Command { & src\xunit.console\bin\Debug\net452\win7-x86\xunit.console.exe test\test.harness\bin\Debug\net452\test.harness.dll -class Foo }).TotalMilliseconds
+
+Write-Host -Foreground Yellow -NoNewLine "dotnet vstest (netcore1.0):  "
+Write-Host (Measure-Command { & dotnet vstest /Framework:FrameworkCore10 /Tests:Foo /Settings:test\test.harness\Settings.runsettings test\test.harness\bin\Debug\netcoreapp1.0\test.harness.dll }).TotalMilliseconds
+
+Write-Host -Foreground Yellow -NoNewLine "dotnet xunit (netcore1.0):   "
+Write-Host (Measure-Command { & dotnet exec --depsfile test\test.harness\bin\Debug\netcoreapp1.0\test.harness.deps.json src\xunit.console\bin\Debug\netcoreapp1.0\xunit.console.dll test\test.harness\bin\Debug\netcoreapp1.0\test.harness.dll -class Foo }).TotalMilliseconds
