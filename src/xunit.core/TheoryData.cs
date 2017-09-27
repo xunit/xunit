17bda38115f073ff4f644e20c90d575b9af5b9e9
Paulo Morgado <paulo@paulomorgado.net>
2017-09-27 18:29:42 +0100

    Added TheoryData types with 6, 7, 8, 9 and 10 parameters (#1470)

--- src/xunit.core/TheoryData.cs
+++ src/xunit.core/TheoryData.cs
@@ -136,4 +136,159 @@ namespace Xunit
             AddRow(p1, p2, p3, p4, p5);
         }
     }
+
+    /// <summary>
+    /// Represents a set of data for a theory with 5 parameters. Data can
+    /// be added to the data set using the collection initializer syntax.
+    /// </summary>
+    /// <typeparam name="T1">The first parameter type.</typeparam>
+    /// <typeparam name="T2">The second parameter type.</typeparam>
+    /// <typeparam name="T3">The third parameter type.</typeparam>
+    /// <typeparam name="T4">The fourth parameter type.</typeparam>
+    /// <typeparam name="T5">The fifth parameter type.</typeparam>
+    /// <typeparam name="T6">The sixth parameter type.</typeparam>
+    public class TheoryData<T1, T2, T3, T4, T5, T6> : TheoryData
+    {
+        /// <summary>
+        /// Adds data to the theory data set.
+        /// </summary>
+        /// <param name="p1">The first data value.</param>
+        /// <param name="p2">The second data value.</param>
+        /// <param name="p3">The third data value.</param>
+        /// <param name="p4">The fourth data value.</param>
+        /// <param name="p5">The fifth data value.</param>
+        /// <param name="p6">The sixth data value.</param>
+        public void Add(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6)
+        {
+            AddRow(p1, p2, p3, p4, p5, p6);
+        }
+    }
+
+    /// <summary>
+    /// Represents a set of data for a theory with 5 parameters. Data can
+    /// be added to the data set using the collection initializer syntax.
+    /// </summary>
+    /// <typeparam name="T1">The first parameter type.</typeparam>
+    /// <typeparam name="T2">The second parameter type.</typeparam>
+    /// <typeparam name="T3">The third parameter type.</typeparam>
+    /// <typeparam name="T4">The fourth parameter type.</typeparam>
+    /// <typeparam name="T5">The fifth parameter type.</typeparam>
+    /// <typeparam name="T6">The sixth parameter type.</typeparam>
+    /// <typeparam name="T7">The seventh parameter type.</typeparam>
+    public class TheoryData<T1, T2, T3, T4, T5, T6, T7> : TheoryData
+    {
+        /// <summary>
+        /// Adds data to the theory data set.
+        /// </summary>
+        /// <param name="p1">The first data value.</param>
+        /// <param name="p2">The second data value.</param>
+        /// <param name="p3">The third data value.</param>
+        /// <param name="p4">The fourth data value.</param>
+        /// <param name="p5">The fifth data value.</param>
+        /// <param name="p6">The sixth data value.</param>
+        /// <param name="p7">The seventh data value.</param>
+        public void Add(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7)
+        {
+            AddRow(p1, p2, p3, p4, p5, p6, p7);
+        }
+    }
+
+    /// <summary>
+    /// Represents a set of data for a theory with 5 parameters. Data can
+    /// be added to the data set using the collection initializer syntax.
+    /// </summary>
+    /// <typeparam name="T1">The first parameter type.</typeparam>
+    /// <typeparam name="T2">The second parameter type.</typeparam>
+    /// <typeparam name="T3">The third parameter type.</typeparam>
+    /// <typeparam name="T4">The fourth parameter type.</typeparam>
+    /// <typeparam name="T5">The fifth parameter type.</typeparam>
+    /// <typeparam name="T6">The sixth parameter type.</typeparam>
+    /// <typeparam name="T7">The seventh parameter type.</typeparam>
+    /// <typeparam name="T8">The eigth parameter type.</typeparam>
+    public class TheoryData<T1, T2, T3, T4, T5, T6, T7, T8> : TheoryData
+    {
+        /// <summary>
+        /// Adds data to the theory data set.
+        /// </summary>
+        /// <param name="p1">The first data value.</param>
+        /// <param name="p2">The second data value.</param>
+        /// <param name="p3">The third data value.</param>
+        /// <param name="p4">The fourth data value.</param>
+        /// <param name="p5">The fifth data value.</param>
+        /// <param name="p6">The sixth data value.</param>
+        /// <param name="p7">The seventh data value.</param>
+        /// <param name="p8">The eigth data value.</param>
+        public void Add(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8)
+        {
+            AddRow(p1, p2, p3, p4, p5, p6, p7, p8);
+        }
+    }
+
+    /// <summary>
+    /// Represents a set of data for a theory with 5 parameters. Data can
+    /// be added to the data set using the collection initializer syntax.
+    /// </summary>
+    /// <typeparam name="T1">The first parameter type.</typeparam>
+    /// <typeparam name="T2">The second parameter type.</typeparam>
+    /// <typeparam name="T3">The third parameter type.</typeparam>
+    /// <typeparam name="T4">The fourth parameter type.</typeparam>
+    /// <typeparam name="T5">The fifth parameter type.</typeparam>
+    /// <typeparam name="T6">The sixth parameter type.</typeparam>
+    /// <typeparam name="T7">The seventh parameter type.</typeparam>
+    /// <typeparam name="T8">The eigth parameter type.</typeparam>
+    /// <typeparam name="T9">The nineth parameter type.</typeparam>
+    public class TheoryData<T1, T2, T3, T4, T5, T6, T7, T8, T9> : TheoryData
+    {
+        /// <summary>
+        /// Adds data to the theory data set.
+        /// </summary>
+        /// <param name="p1">The first data value.</param>
+        /// <param name="p2">The second data value.</param>
+        /// <param name="p3">The third data value.</param>
+        /// <param name="p4">The fourth data value.</param>
+        /// <param name="p5">The fifth data value.</param>
+        /// <param name="p6">The sixth data value.</param>
+        /// <param name="p7">The seventh data value.</param>
+        /// <param name="p8">The eigth data value.</param>
+        /// <param name="p9">The nineth data value.</param>
+        public void Add(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9)
+        {
+            AddRow(p1, p2, p3, p4, p5, p6, p7, p8, p9);
+        }
+    }
+
+    /// <summary>
+    /// Represents a set of data for a theory with 5 parameters. Data can
+    /// be added to the data set using the collection initializer syntax.
+    /// </summary>
+    /// <typeparam name="T1">The first parameter type.</typeparam>
+    /// <typeparam name="T2">The second parameter type.</typeparam>
+    /// <typeparam name="T3">The third parameter type.</typeparam>
+    /// <typeparam name="T4">The fourth parameter type.</typeparam>
+    /// <typeparam name="T5">The fifth parameter type.</typeparam>
+    /// <typeparam name="T6">The sixth parameter type.</typeparam>
+    /// <typeparam name="T7">The seventh parameter type.</typeparam>
+    /// <typeparam name="T8">The eigth parameter type.</typeparam>
+    /// <typeparam name="T9">The nineth parameter type.</typeparam>
+    /// <typeparam name="T10">The tenth parameter type.</typeparam>
+    public class TheoryData<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : TheoryData
+    {
+        /// <summary>
+        /// Adds data to the theory data set.
+        /// </summary>
+        /// <param name="p1">The first data value.</param>
+        /// <param name="p2">The second data value.</param>
+        /// <param name="p3">The third data value.</param>
+        /// <param name="p4">The fourth data value.</param>
+        /// <param name="p5">The fifth data value.</param>
+        /// <param name="p6">The sixth data value.</param>
+        /// <param name="p7">The seventh data value.</param>
+        /// <param name="p8">The eigth data value.</param>
+        /// <param name="p9">The nineth data value.</param>
+        /// <param name="p10">The tenth data value.</param>
+        public void Add(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10)
+        {
+            AddRow(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
+        }
+    }
 }
