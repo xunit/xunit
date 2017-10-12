936c27dc53cf5b868505127cd78a62eb1795e6f0
Brad Wilson <dotnetguy@gmail.com>
2017-10-12 13:27:21 -0700

    Removed v1 extensions tests which no longer pass

--- test/test.xunit1/xunit.extensions/DataTheories/TheoryAttributeTests.cs
+++ test/test.xunit1/xunit.extensions/DataTheories/TheoryAttributeTests.cs
@@ -11,40 +11,6 @@ namespace Xunit1.Extensions
     public class TheoryAttributeTests
     {
         [Fact]
-        public void TestDataFromOleDb()
-        {
-            if (IntPtr.Size == 8)  // Test always fails in 64-bit; no JET engine
-                return;
-
-            string currentDirectory = Directory.GetCurrentDirectory();
-
-            try
-            {
-                string executable = Assembly.GetExecutingAssembly().GetLocalCodeBase();
-                Directory.SetCurrentDirectory(Path.GetDirectoryName(executable));
-                MethodInfo method = typeof(TestMethodCommandClass).GetMethod("TestViaOleDb");
-                TheoryAttribute attr = (TheoryAttribute)(method.GetCustomAttributes(typeof(TheoryAttribute), false))[0];
-
-                List<ITestCommand> commands = new List<ITestCommand>(attr.CreateTestCommands(Reflector.Wrap(method)));
-
-                Assert.Equal(2, commands.Count);
-                TheoryCommand command1 = Assert.IsType<TheoryCommand>(commands[0]);
-                Assert.Equal(3, command1.Parameters.Length);
-                Assert.Equal<object>(1D, command1.Parameters[0]);
-                Assert.Equal<object>("Foo", command1.Parameters[1]);
-                Assert.Equal<object>("Bar", command1.Parameters[2]);
-                TheoryCommand command2 = Assert.IsType<TheoryCommand>(commands[1]);
-                Assert.Equal(3, command2.Parameters.Length);
-                Assert.Equal<object>(14D, command2.Parameters[0]);
-                Assert.Equal<object>("Biff", command2.Parameters[1]);
-                Assert.Equal<object>("Baz", command2.Parameters[2]);
-            }
-            finally
-            {
-                Directory.SetCurrentDirectory(currentDirectory);
-            }
-        }
-        [Fact]
         public void TestDataFromProperty()
         {
             MethodInfo method = typeof(TestMethodCommandClass).GetMethod("TestViaProperty");
@@ -84,30 +50,6 @@ namespace Xunit1.Extensions
             TheoryCommand theoryCommand = Assert.IsType<TheoryCommand>(command);
             object parameter = Assert.Single(theoryCommand.Parameters);
             Assert.Equal(3, parameter);
-        }
-
-        [Fact]
-        public void TestDataFromXls()
-        {
-            if (IntPtr.Size == 8)  // Test always fails in 64-bit; no JET engine
-                return;
-
-            MethodInfo method = typeof(TestMethodCommandClass).GetMethod("TestViaXls");
-            TheoryAttribute attr = (TheoryAttribute)(method.GetCustomAttributes(typeof(TheoryAttribute), false))[0];
-
-            List<ITestCommand> commands = new List<ITestCommand>(attr.CreateTestCommands(Reflector.Wrap(method)));
-
-            Assert.Equal(2, commands.Count);
-            TheoryCommand command1 = Assert.IsType<TheoryCommand>(commands[0]);
-            Assert.Equal(3, command1.Parameters.Length);
-            Assert.Equal<object>(1D, command1.Parameters[0]);
-            Assert.Equal<object>("Foo", command1.Parameters[1]);
-            Assert.Equal<object>("Bar", command1.Parameters[2]);
-            TheoryCommand command2 = Assert.IsType<TheoryCommand>(commands[1]);
-            Assert.Equal(3, command2.Parameters.Length);
-            Assert.Equal<object>(14D, command2.Parameters[0]);
-            Assert.Equal<object>("Biff", command2.Parameters[1]);
-            Assert.Equal<object>("Baz", command2.Parameters[2]);
         }
 
         [Fact]
@@ -157,11 +99,6 @@ namespace Xunit1.Extensions
                 get { yield return new object[] { 2 }; }
             }
 
-            [Theory, OleDbData(
-                @"Provider=Microsoft.Jet.OleDb.4.0; Data Source=xunit.extensions\DataTheories\UnitTestData.xls; Extended Properties=Excel 8.0",
-                "SELECT x, y, z FROM Data")]
-            public void TestViaOleDb(double x, string y, string z) { }
-
             [Theory, PropertyData("TheoryDataProperty")]
             public void TestViaProperty(int x) { }
 
@@ -170,9 +107,6 @@ namespace Xunit1.Extensions
 
             [Theory, PropertyData("TheoryDataProperty", PropertyType = typeof(ExternalData))]
             public void TestViaOtherTypeProperty(int x) { }
-
-            [Theory, ExcelData(@"xunit.extensions\DataTheories\UnitTestData.xls", "SELECT x, y, z FROM Data")]
-            public void TestViaXls(double x, string y, string z) { }
 
             [Theory, PropertyData("GenericData")]
             public void GenericTest<T>(T value) { }
