fafc450c522fcd994764ad6645f4db74f96a9f8a
Adam Ralph <adam@adamralph.com>
2017-11-03 16:42:01 +0000

    red: added ReflectorTests.Conversion.ConvertsStringToGuid

--- test/test.xunit.execution/Sdk/Reflection/ReflectorTests.cs
+++ test/test.xunit.execution/Sdk/Reflection/ReflectorTests.cs
@@ -7,6 +7,25 @@ using Xunit.Sdk;
 
 public class ReflectorTests
 {
+    public class Conversion
+    {
+        [Theory]
+        [InlineData("{5B21E154-15EB-4B1E-BC30-127E8A41ECA1}")]
+        [InlineData("4EBCD32C-A2B8-4600-9E72-3873347E285C")]
+        [InlineData("39A3B4C85FEF43A988EB4BB4AC4D4103")]
+        [InlineData("{5b21e154-15eb-4b1e-bc30-127e8a41eca1}")]
+        [InlineData("4ebcd32c-a2b8-4600-9e72-3873347e285c")]
+        [InlineData("39a3b4c85fef43a988eb4bb4ac4d4103")]
+        public void ConvertsStringToGuid(string text)
+        {
+            var guid = Guid.Parse(text);
+
+            var args = Reflector.ConvertArguments(new[] { text }, new[] { typeof(Guid) });
+
+            Assert.Equal(guid, Assert.IsType<Guid>(args[0]));
+        }
+    }
+
     public class Undecorated // Defaults, inherited = true, multiple = false
     {
         class AttributeUnderTest : Attribute
