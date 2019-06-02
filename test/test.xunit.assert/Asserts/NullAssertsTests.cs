c31eb72f03a6724156a18a789ec894d3fd9388cd
Corentin Altepe <corentin.altepe@gmail.com>
2019-06-02 14:54:09 +0200

    Added a test case in attempt to reproduce issue #1932

--- test/test.xunit.assert/Asserts/NullAssertsTests.cs
+++ test/test.xunit.assert/Asserts/NullAssertsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xunit;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 public class NullAssertsTests
@@ -24,6 +25,12 @@ public class NullAssertsTests
 
     public class Null
     {
+        private readonly ITestOutputHelper Output;
+        public Null(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
         [Fact]
         public void Success()
         {
@@ -40,6 +47,23 @@ public class NullAssertsTests
             Assert.Equal("Assert.Null() Failure" + Environment.NewLine +
                          "Expected: (null)" + Environment.NewLine +
                          "Actual:   Object { }", ex.Message);
+        }
+
+        /// <summary>
+        /// Test case specific to the issue #1932
+        /// </summary>
+        [Fact]
+        public void PrintControlCharactersSuccess()
+        {
+            var ex = Record.Exception(
+                () => Assert.Null("\0*.*\0"));
+            
+            Assert.IsType<NullException>(ex);
+            Assert.Equal("Assert.Null() Failure" + Environment.NewLine +
+                         "Expected: (null)" + Environment.NewLine +
+                         "Actual:   \0*.*\0", ex.Message);
+
+            Output.WriteLine(ex.Message);
         }
     }
 }
