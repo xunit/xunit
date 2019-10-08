08b2105ccd064d8cc1ddfdacfdac02d2f4ce157d
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-10-08 09:51:02 +0200

    Add tests for Assert.Fail().
    This partially resolves https://github.com/xunit/xunit/issues/2027.

---
+++ test/test.xunit.assert/Asserts/FailAssertsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace test.xunit.assert.Asserts
+{
+    public class FailAssertsTests
+    {
+        public class Fail
+        {
+            [Fact]
+            public void Failure()
+            {
+                var ex = Record.Exception(() => Assert.Fail());
+
+                Assert.IsType<FailException>(ex);
+                Assert.Equal("Assert.Fail() Failure", ex.Message);
+            }
+
+            [Fact]
+            public void UserSuppliedMessage()
+            {
+                var ex = Record.Exception(() => Assert.Fail("Custom user message"));
+
+                Assert.IsType<FailException>(ex);
+                Assert.Equal("Custom user message", ex.Message);
+            }
+        }
+    }
+}
