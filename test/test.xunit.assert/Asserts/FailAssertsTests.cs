c6754660440063b3f79c7634bfbd38b7037d7c3f
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-10-31 11:52:53 +0100

    Remove namespace declaration.

--- test/test.xunit.assert/Asserts/FailAssertsTests.cs
+++ test/test.xunit.assert/Asserts/FailAssertsTests.cs
@@ -6,29 +6,26 @@ using System.Threading.Tasks;
 using Xunit;
 using Xunit.Sdk;
 
-namespace test.xunit.assert.Asserts
+public class FailAssertsTests
 {
-    public class FailAssertsTests
+    public class Fail
     {
-        public class Fail
+        [Fact]
+        public void Failure()
         {
-            [Fact]
-            public void Failure()
-            {
-                var ex = Record.Exception(() => Assert.Fail());
+            var ex = Record.Exception(() => Assert.Fail());
 
-                Assert.IsType<FailException>(ex);
-                Assert.Equal("Assert.Fail() Failure", ex.Message);
-            }
+            Assert.IsType<FailException>(ex);
+            Assert.Equal("Assert.Fail() Failure", ex.Message);
+        }
 
-            [Fact]
-            public void UserSuppliedMessage()
-            {
-                var ex = Record.Exception(() => Assert.Fail("Custom user message"));
+        [Fact]
+        public void UserSuppliedMessage()
+        {
+            var ex = Record.Exception(() => Assert.Fail("Custom user message"));
 
-                Assert.IsType<FailException>(ex);
-                Assert.Equal("Custom user message", ex.Message);
-            }
+            Assert.IsType<FailException>(ex);
+            Assert.Equal("Custom user message", ex.Message);
         }
     }
 }
