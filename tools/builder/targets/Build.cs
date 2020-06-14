53c453dd0c05d87f0fc7fbe2efe1521989e0aced
Brad Wilson <dotnetguy@gmail.com>
2020-06-14 00:36:09 -0700

    Move submodule update to the restore step

--- tools/builder/targets/Build.cs
+++ tools/builder/targets/Build.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using System.Threading.Tasks;
 
 [Target(
@@ -10,11 +9,6 @@ public static class Build
     public static async Task OnExecute(BuildContext context)
     {
         context.BuildStep("Compiling binaries");
-
-        var assertFiles = Directory.GetFiles(Path.Combine(context.BaseFolder, "src", "xunit.v3.assert", "Asserts"));
-        var mediaFiles = Directory.GetFiles(Path.Combine(context.BaseFolder, "tools", "media"));
-        if (assertFiles.Length == 0 || mediaFiles.Length == 0)
-            await context.Exec("git", "submodule update --init");
 
         await context.Exec("dotnet", $"build --no-restore --configuration {context.ConfigurationText} --verbosity {context.Verbosity}");
         // await context.Exec("dotnet", $"msbuild src/xunit.console/xunit.console.csproj -p:Configuration={context.ConfigurationText} -p:Platform=x86");
--- tools/builder/targets/Restore.cs
+++ tools/builder/targets/Restore.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 
 [Target(BuildTarget.Restore)]
@@ -5,6 +6,15 @@ public static class Restore
 {
     public static async Task OnExecute(BuildContext context)
     {
+        var assertFiles = Directory.GetFiles(Path.Combine(context.BaseFolder, "src", "xunit.v3.assert", "Asserts"));
+        var mediaFiles = Directory.GetFiles(Path.Combine(context.BaseFolder, "tools", "media"));
+        if (assertFiles.Length == 0 || mediaFiles.Length == 0)
+        {
+            context.BuildStep("Updating submodules");
+
+            await context.Exec("git", "submodule update --init");
+        }
+
         context.BuildStep("Restoring NuGet packages");
 
         await context.Exec("dotnet", $"restore --verbosity {context.Verbosity}");
