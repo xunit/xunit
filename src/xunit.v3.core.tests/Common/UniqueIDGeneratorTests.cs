4cd6181cee250ff6ecb5bb9dbd81203b31fcde0f
Brad Wilson <dotnetguy@gmail.com>
2020-09-14 21:31:35 -0700

    Externalize unique ID generator, use built-in SHA256 instead of copied SHA1

---
+++ src/xunit.v3.core.tests/Common/UniqueIDGeneratorTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xunit;
+
+public class UniqueIDGeneratorTests
+{
+	[Fact]
+	public void GuardClause()
+	{
+		using var generator = new UniqueIDGenerator();
+
+		var ex = Record.Exception(() => generator.Add(null!));
+
+		var anex = Assert.IsType<ArgumentNullException>(ex);
+		Assert.Equal("value", anex.ParamName);
+	}
+
+	[Fact]
+	public void NoData()
+	{
+		using var generator = new UniqueIDGenerator();
+
+		var result = generator.Compute();
+
+		Assert.Equal("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", result);
+	}
+
+	[Fact]
+	public void SingleString()
+	{
+		using var generator = new UniqueIDGenerator();
+		generator.Add("Hello, world!");
+
+		var result = generator.Compute();
+
+		Assert.Equal("5450bb49d375ba935c1fe9c4dc48775d7d343fb97f22f07f8950f34a75a2708f", result);
+	}
+
+	[Fact]
+	public void CannotAddAfterCompute()
+	{
+		using var generator = new UniqueIDGenerator();
+		generator.Compute();
+
+		var ex = Record.Exception(() => generator.Add("Hello, world!"));
+
+		Assert.IsType<ObjectDisposedException>(ex);
+		Assert.StartsWith("Cannot use UniqueIDGenerator after you have called Compute or Dispose", ex.Message);
+	}
+
+	[Fact]
+	public void CannotComputeTwice()
+	{
+		using var generator = new UniqueIDGenerator();
+		generator.Compute();
+
+		var ex = Record.Exception(() => generator.Compute());
+
+		Assert.IsType<ObjectDisposedException>(ex);
+		Assert.StartsWith("Cannot use UniqueIDGenerator after you have called Compute or Dispose", ex.Message);
+	}
+
+	[Fact]
+	public void CannotAddAfterDipose()
+	{
+		using var generator = new UniqueIDGenerator();
+		generator.Dispose();
+
+		var ex = Record.Exception(() => generator.Add("Hello, world!"));
+
+		Assert.IsType<ObjectDisposedException>(ex);
+		Assert.StartsWith("Cannot use UniqueIDGenerator after you have called Compute or Dispose", ex.Message);
+	}
+
+	[Fact]
+	public void CannotComputeAfterDispose()
+	{
+		using var generator = new UniqueIDGenerator();
+		generator.Dispose();
+
+		var ex = Record.Exception(() => generator.Compute());
+
+		Assert.IsType<ObjectDisposedException>(ex);
+		Assert.StartsWith("Cannot use UniqueIDGenerator after you have called Compute or Dispose", ex.Message);
+	}
+}
--- src/xunit.v3.core/Sdk/Frameworks/TestMethodTestCase.cs
+++ src/xunit.v3.core/Sdk/Frameworks/TestMethodTestCase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -216,61 +215,26 @@ namespace Xunit.Sdk
 		/// </summary>
 		protected virtual string GetUniqueID()
 		{
-			using var stream = new MemoryStream();
+			using var idGenerator = new UniqueIDGenerator();
 			var assemblyName = TestMethod.TestClass.TestCollection.TestAssembly.Assembly.Name;
 
 			// Get just the assembly name (without version info) when obtained by reflection
 			if (TestMethod.TestClass.TestCollection.TestAssembly.Assembly is IReflectionAssemblyInfo assembly)
 				assemblyName = assembly.Assembly.GetName().Name ?? assemblyName;
 
-			Write(stream, assemblyName);
-			Write(stream, TestMethod.TestClass.Class.Name);
-			Write(stream, TestMethod.Method.Name);
+			idGenerator.Add(assemblyName);
+			idGenerator.Add(TestMethod.TestClass.Class.Name);
+			idGenerator.Add(TestMethod.Method.Name);
 
 			if (TestMethodArguments != null)
-				Write(stream, SerializationHelper.Serialize(TestMethodArguments));
+				idGenerator.Add(SerializationHelper.Serialize(TestMethodArguments));
 
 			var genericTypes = MethodGenericTypes;
 			if (genericTypes != null)
 				for (var idx = 0; idx < genericTypes.Length; idx++)
-					Write(stream, TypeUtility.ConvertToSimpleTypeName(genericTypes[idx]));
+					idGenerator.Add(TypeUtility.ConvertToSimpleTypeName(genericTypes[idx]));
 
-			stream.Position = 0;
-
-			var hash = new byte[20];
-			var data = stream.ToArray();
-
-			var hasher = new Sha1Digest();
-			hasher.BlockUpdate(data, 0, data.Length);
-			hasher.DoFinal(hash, 0);
-
-			return BytesToHexString(hash);
-		}
-
-		/// <summary>Converts an array of bytes to its hexadecimal value as a string.</summary>
-		/// <param name="bytes">The bytes.</param>
-		/// <returns>A string containing the hexadecimal representation of the provided bytes.</returns>
-		static string BytesToHexString(byte[] bytes)
-		{
-			var chars = new char[bytes.Length * 2];
-			var i = 0;
-
-			foreach (var b in bytes)
-			{
-				chars[i++] = NibbleToHexChar(b >> 4);
-				chars[i++] = NibbleToHexChar(b & 0xF);
-			}
-
-			return new string(chars);
-		}
-
-		/// <summary>Gets a hexadecimal digit character from the 4-bit value.</summary>
-		/// <param name="b">A value in the range [0, 15].</param>
-		/// <returns>A character in the range ['0','9'] or ['a','f'].</returns>
-		static char NibbleToHexChar(int b)
-		{
-			Debug.Assert(b < 16);
-			return (char)(b < 10 ? b + '0' : (b - 10) + 'a');
+			return idGenerator.Compute();
 		}
 
 		/// <summary>
