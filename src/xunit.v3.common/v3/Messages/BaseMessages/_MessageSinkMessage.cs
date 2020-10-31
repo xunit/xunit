2007085caccc19016e08c1015b6fefb12c98aa98
Brad Wilson <dotnetguy@gmail.com>
2020-10-31 14:30:46 -0700

    Decorate _MessageSinkMessage with IMessageSinkMessage to temporarily ease the conversion from v2 => v3 messages

--- src/xunit.v3.common/v3/Messages/BaseMessages/_MessageSinkMessage.cs
+++ src/xunit.v3.common/v3/Messages/BaseMessages/_MessageSinkMessage.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using Xunit.Abstractions;
 using Xunit.Internal;
 
 namespace Xunit.v3
@@ -12,7 +13,8 @@ namespace Xunit.v3
 	/// The base type for all messages. It provides serialization support. Because of the way type
 	/// serialization works, messages can only be defined by xUnit.net itself.
 	/// </summary>
-	public class _MessageSinkMessage
+	// TODO: Remove IMessageSinkMessage once we're separated from v2 messsages and can update _IMessageSink.
+	public class _MessageSinkMessage : IMessageSinkMessage
 	{
 		delegate void PropertyWriter(Utf8JsonWriter writer, object? value, JsonSerializerOptions options);
 
