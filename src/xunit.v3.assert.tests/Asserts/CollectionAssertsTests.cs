89e9dbd31ce290f7d0bf272b326d07f078703666
Louis Zanella <louis.zanella@gmail.com>
2020-11-21 16:13:44 -0500

    Add test to validate new dictionary output format (#2191)

--- src/xunit.v3.assert.tests/Asserts/CollectionAssertsTests.cs
+++ src/xunit.v3.assert.tests/Asserts/CollectionAssertsTests.cs
@@ -900,6 +900,38 @@ public class CollectionAssertsTests
 			Assert.NotEqual(expected, actual);
 			Assert.Throws<EqualException>(() => Assert.Equal(expected, actual));
 		}
+
+		[Fact]
+		public static void SomeKeysDiffer()
+		{
+			var expected = new Dictionary<string, int>
+			{
+				["a"] = 1,
+				["be"] = 2,
+				["c"] = 3,
+				["d"] = 4,
+				["e"] = 5,
+				["f"] = 6,
+			};
+			var actual = new Dictionary<string, int>
+			{
+				["a"] = 1,
+				["ba"] = 2,
+				["c"] = 3,
+				["d"] = 4,
+				["e"] = 5,
+				["f"] = 6,
+			};
+
+			Assert.NotEqual(expected, actual);
+			var ex = Assert.Throws<EqualException>(() => Assert.Equal(expected, actual));
+			Assert.Equal(
+				"Assert.Equal() Failure" + Environment.NewLine +
+				"Expected: Dictionary<String, Int32> [[\"a\"] = 1, [\"be\"] = 2, [\"c\"] = 3, [\"d\"] = 4, [\"e\"] = 5, ...]" + Environment.NewLine +
+				"Actual:   Dictionary<String, Int32> [[\"a\"] = 1, [\"ba\"] = 2, [\"c\"] = 3, [\"d\"] = 4, [\"e\"] = 5, ...]",
+				ex.Message
+			);
+		}
 	}
 
 	public class EqualSet
