9d453effb85e1c97ec27103f792a1286f42edf36
Mark Brett <Xcodo@users.noreply.github.com>
2021-06-28 20:33:44 +0100

    Add equality overrides for _NullMessageSink

--- src/xunit.v3.common/v3/_NullMessageSink.cs
+++ src/xunit.v3.common/v3/_NullMessageSink.cs
@@ -7,5 +7,35 @@ namespace Xunit.v3
 	{
 		/// <inheritdoc/>
 		public bool OnMessage(_MessageSinkMessage message) => true;
+
+		/// <inheritdoc/>
+		public override bool Equals(object? obj)
+		{
+			return (obj is _NullMessageSink);
+		}
+
+		/// <inheritdoc/>
+		public bool Equals(_NullMessageSink other)
+		{
+			return (other is _NullMessageSink);
+		}
+
+		/// <inheritdoc/>
+		public static bool operator ==(_NullMessageSink lhs, _NullMessageSink rhs)
+		{
+			return (lhs is _NullMessageSink && rhs is _NullMessageSink);
+		}
+
+		/// <inheritdoc/>
+		public static bool operator !=(_NullMessageSink lhs, _NullMessageSink rhs)
+		{
+			return !(lhs == rhs);
+		}
+
+		/// <inheritdoc/>
+		public override int GetHashCode()
+		{
+			return typeof(_NullMessageSink).GetHashCode();
+		}
 	}
 }
