2f6f7f07724de847805dab26a7c502a87b3e3005
Brad Wilson <dotnetguy@gmail.com>
2021-08-14 12:37:27 -0700

    Sort execution summary by assembly display name

--- src/xunit.v3.runner.common.tests/Reporters/DefaultRunnerReporterMessageHandlerTests.cs
+++ src/xunit.v3.runner.common.tests/Reporters/DefaultRunnerReporterMessageHandlerTests.cs
@@ -354,9 +354,9 @@ public class DefaultRunnerReporterMessageHandlerTests
 
 			Assert.Collection(handler.Messages,
 				msg => Assert.Equal("[Imp] => === TEST EXECUTION SUMMARY ===", msg),
-				msg => Assert.Equal("[Imp] =>    short       Total:  2112, Errors:  6, Failed:  42, Skipped:  8, Time: 1.235s", msg),
-				msg => Assert.Equal("[Imp] =>    nothing     Total:     0", msg),
 				msg => Assert.Equal("[Imp] =>    longerName  Total: 10240, Errors:  7, Failed:  96, Skipped:  4, Time: 3.457s", msg),
+				msg => Assert.Equal("[Imp] =>    nothing     Total:     0", msg),
+				msg => Assert.Equal("[Imp] =>    short       Total:  2112, Errors:  6, Failed:  42, Skipped:  8, Time: 1.235s", msg),
 				msg => Assert.Equal("[Imp] =>                       -----          --          ---           --        ------", msg),
 				msg => Assert.Equal("[Imp] =>          GRAND TOTAL: 12352          13          138           12        4.691s (12.346s)", msg)
 			);
