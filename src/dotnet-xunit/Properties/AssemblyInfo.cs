f907e78235a8943129479176fd1066f61b645d16
Brad Wilson <dotnetguy@gmail.com>
2021-09-13 16:46:55 -0700

    Remove dotnet-xunit

--- src/dotnet-xunit/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-#if NETCOREAPP1_0
-[assembly: AssemblyTitle("xUnit.net Runner for .NET Core (1.x)")]
-#elif NETCOREAPP2_0
-[assembly: AssemblyTitle("xUnit.net Runner for .NET Core (2.x)")]
-#else
-#error Unknown target platform
-#endif
