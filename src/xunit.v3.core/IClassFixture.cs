516dbc7ccb32cc1f5229b96ba26fe4261e749b9f
Brad Wilson <dotnetguy@gmail.com>
2021-10-23 17:15:33 -0700

    Make XML docs for IClassFixture and ICollectionFixture clearer w/rt IAsyncLifetime and IAsyncDisposable

--- src/xunit.v3.core/IClassFixture.cs
+++ src/xunit.v3.core/IClassFixture.cs
@@ -5,20 +5,16 @@ namespace Xunit
 	/// <summary>
 	/// Used to decorate xUnit.net test classes and collections to indicate a test which has
 	/// per-test-class fixture data. An instance of the fixture data is initialized just before
-	/// the first test in the class is run, and if it implements IDisposable, is disposed
-	/// after the last test in the class is run. To gain access to the fixture data from
-	/// inside the test, a constructor argument should be added to the test class which
-	/// exactly matches the <typeparamref name="TFixture"/>. Class fixtures must have a
-	/// single parameterless constructor, and may take collection fixture types as constructor
-	/// arguments.
+	/// the first test in the class is run (including <see cref="IAsyncLifetime.InitializeAsync"/>
+	/// if it's implemented). After all the tests in the test class have been run, it is cleaned up
+	/// by calling <see cref="IAsyncDisposable.DisposeAsync"/> if it's implemented, or it falls back
+	/// to <see cref="IDisposable.Dispose"/> if that's implemented. Class fixtures may have
+	/// a public constructor which is either empty, or accepts one or more assembly and/or collection
+	/// fixture objects as constructor arguments. To gain access to the fixture data from inside the
+	/// test, a constructor argument should be added to the test class which/ exactly matches the
+	/// <typeparamref name="TFixture"/>.
 	/// </summary>
 	/// <typeparam name="TFixture">The type of the fixture.</typeparam>
-	/// <remarks>
-	/// If asynchronous setup of <typeparamref name="TFixture"/> is required
-	/// it should implement the <see cref="IAsyncLifetime"/> interface; if
-	/// asynchronous disposal of <typeparamref name="TFixture"/> is required
-	/// it should implement the <see cref="IAsyncDisposable"/> interface.
-	/// </remarks>
 	public interface IClassFixture<TFixture>
 		where TFixture : class
 	{ }
