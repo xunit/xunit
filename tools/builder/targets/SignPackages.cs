73a2356e66877f182c41cc62e2094ba08ade49b1
Brad Wilson <dotnetguy@gmail.com>
2021-11-08 12:42:19 -0800

    Use globbing (and long arg names) for package signing

--- tools/builder/targets/SignPackages.cs
+++ tools/builder/targets/SignPackages.cs
@@ -23,20 +23,12 @@ public static class SignPackages
 			return;
 		}
 
-		var packageFiles =
-			Directory
-				.GetFiles(context.PackageOutputFolder, "*.nupkg", SearchOption.AllDirectories)
-				.Select(x => x.Substring(context.BaseFolder.Length + 1));
-
 		var signClientAppSettings = Path.Combine(context.BaseFolder, "tools", "SignClient", "appsettings.json");
-		foreach (var packageFile in packageFiles.OrderBy(x => x))
-		{
-			var args = $"SignClient sign -c \"{signClientAppSettings}\" -r \"{signClientUser}\" -s \"{signClientSecret}\" -n \"xUnit.net\" -d \"xUnit.net\" -u \"https://github.com/xunit/xunit\" -i \"{packageFile}\"";
-			var redactedArgs =
-				args.Replace(signClientUser, "[redacted]")
-					.Replace(signClientSecret, "[redacted]");
+		var args = $"SignClient sign --config \"{signClientAppSettings}\" --user \"{signClientUser}\" --secret \"{signClientSecret}\" --name \"xUnit.net\" --description \"xUnit.net\" -u \"https://github.com/xunit/xunit\" --baseDirectory \"{context.PackageOutputFolder}\" --input **/*.nupkg";
+		var redactedArgs =
+			args.Replace(signClientUser, "[redacted]")
+				.Replace(signClientSecret, "[redacted]");
 
-			await context.Exec("dotnet", args, redactedArgs);
-		}
+		await context.Exec("dotnet", args, redactedArgs);
 	}
 }
