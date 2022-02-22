7d9cb8a771676d874d9ddfb55e11f17dff655ba1
robo <ronny.borchert@indu-sol.com>
2022-02-22 14:29:37 +0100

    add test for IEnumerable with additional properties

--- src/xunit.v1.tests/xunit/NotEqualTests.cs
+++ src/xunit.v1.tests/xunit/NotEqualTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Xunit.Sdk;
 
@@ -90,6 +92,45 @@ namespace Xunit1
 			public int GetHashCode(int obj)
 			{
 				throw new NotImplementedException();
+			}
+		}
+
+		public class EnumerableWithAdditionsTests
+		{
+			[Fact]
+			public void AssertEqualWithDoubleWithPrecision()
+			{
+				var first = new ComponentObject { Name = "1" };
+				var second = new ComponentObject { Name = "2" };
+
+				Assert.NotEqual(first, second);
+			}
+
+			class ComponentObject : IEnumerable<object>
+			{
+				private readonly HashSet<object> _decorationValues = new HashSet<object>();
+
+				public string Name { get; set; }
+
+				public IEnumerator<object> GetEnumerator()
+				{
+					return _decorationValues.GetEnumerator();
+				}
+
+				IEnumerator IEnumerable.GetEnumerator()
+				{
+					return GetEnumerator();
+				}
+
+				public override bool Equals(object obj)
+				{
+					return obj is ComponentObject other && Name == other.Name && _decorationValues.SequenceEqual(other._decorationValues);
+				}
+
+				public override int GetHashCode()
+				{
+					throw new NotImplementedException();
+				}
 			}
 		}
 	}
