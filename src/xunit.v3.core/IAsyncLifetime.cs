12026778465c166f94521ea634bc923cf05cb3f1
Brad Wilson <dotnetguy@gmail.com>
2022-07-24 17:45:45 -0700

    Update documentation on IAsyncLifetime to mention AssemblyFixture

--- src/xunit.v3.core/IAsyncLifetime.cs
+++ src/xunit.v3.core/IAsyncLifetime.cs
@@ -4,10 +4,11 @@ using System.Threading.Tasks;
 namespace Xunit;
 
 /// <summary>
-/// Used to provide asynchronous lifetime functionality. Currently supported:
-/// - Test classes
-/// - Classes used in <see cref="IClassFixture{TFixture}"/>
-/// - Classes used in <see cref="ICollectionFixture{TFixture}"/>.
+/// Used to provide asynchronous lifetime functionality. Currently supported:<br />
+/// - Test classes<br />
+/// - Classes used in <see cref="IClassFixture{TFixture}"/><br />
+/// - Classes used in <see cref="ICollectionFixture{TFixture}"/>.<br />
+/// - Classes used in <c>[assembly: <see cref="AssemblyFixtureAttribute"/>()]</c>.
 /// </summary>
 public interface IAsyncLifetime : IAsyncDisposable
 {
