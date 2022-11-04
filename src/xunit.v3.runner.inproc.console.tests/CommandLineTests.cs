62ae1453aaa5d786a621f914b86ee8846c506a4b
Eli Entelis <eli.entelis@hexagon.com>
2022-11-04 23:40:55 +0200

    Switches class refuctored to inherit from IDispoable; on initialize save the enviroment variable "NO_COLOR" and on dispose reset its value

--- src/xunit.v3.runner.inproc.console.tests/CommandLineTests.cs
+++ src/xunit.v3.runner.inproc.console.tests/CommandLineTests.cs
@@ -83,8 +83,15 @@ public class CommandLineTests
 		}
 	}
 
-	public class Switches
+	[Collection("Switches Test Collection")]
+	public class Switches : IDisposable
 	{
+		private readonly string? _originalNoColorValue;
+
+		public Switches()
+		{
+			_originalNoColorValue = Environment.GetEnvironmentVariable("NO_COLOR");
+		}
 		static readonly (string Switch, Expression<Func<XunitProject, bool>> Accessor)[] SwitchOptionsList = new (string, Expression<Func<XunitProject, bool>>)[]
 		{
 			("-debug", project => project.Configuration.DebugOrDefault),
@@ -110,11 +117,10 @@ public class CommandLineTests
 		[Theory(DisableDiscoveryEnumeration = true)]
 		[MemberData(nameof(SwitchesLowerCase))]
 		[MemberData(nameof(SwitchesUpperCase))]
-		public static void SwitchDefault(
+		public void SwitchDefault(
 			string _,
 			Expression<Func<XunitProject, bool>> accessor)
 		{
-			//Environment.SetEnvironmentVariable("NO_COLOR", null);
 			var commandLine = new TestableCommandLine("no-config.json");
 			var project = commandLine.Parse();
 
@@ -126,7 +132,7 @@ public class CommandLineTests
 		[Theory(DisableDiscoveryEnumeration = true)]
 		[MemberData(nameof(SwitchesLowerCase))]
 		[MemberData(nameof(SwitchesUpperCase))]
-		public static void SwitchOverride(
+		public void SwitchOverride(
 			string @switch,
 			Expression<Func<XunitProject, bool>> accessor)
 		{
@@ -136,6 +142,11 @@ public class CommandLineTests
 			var result = accessor.Compile().Invoke(project);
 
 			Assert.True(result);
+		}
+
+		public void Dispose()
+		{
+			Environment.SetEnvironmentVariable("NO_COLOR", _originalNoColorValue);
 		}
 	}
 
