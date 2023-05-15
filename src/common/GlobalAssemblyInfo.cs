eba74cf36b43dd3d0f8495e8eb2faeba4ef6fc58
Brad Wilson <dotnetguy@gmail.com>
2023-05-14 22:47:10 -0700

    #2682: Apply assembly-wide ExcludeFromCodeCoverageAttribute (v2)

--- src/common/GlobalAssemblyInfo.cs
+++ src/common/GlobalAssemblyInfo.cs
@@ -1,13 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 [assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("xUnit.net Testing Framework")]
 [assembly: AssemblyCopyright("Copyright (C) .NET Foundation")]
+[assembly: ExcludeFromCodeCoverage]
 
-#if NET35 || NETCOREAPP1_0
 namespace System.Diagnostics.CodeAnalysis
 {
     [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     internal sealed class ExcludeFromCodeCoverageAttribute : Attribute { }
 }
-#endif
