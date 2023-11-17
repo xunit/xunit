dc28c5de21954f65cb0d2fc6e108b3eb4f10419a
Brad Wilson <dotnetguy@gmail.com>
2023-11-17 11:59:45 -0800

    Add -silent reporter which does not output anything

---
+++ src/xunit.runner.reporters/SilentReporter.cs
@@ -0,0 +1,19 @@
+﻿using Xunit.Abstractions;
+
+namespace Xunit.Runner.Reporters
+{
+    public class SilentReporter : IRunnerReporter
+    {
+        public string Description =>
+            "turns off all output messages";
+
+        public bool IsEnvironmentallyEnabled =>
+            false;
+
+        public string RunnerSwitch =>
+            "silent";
+
+        public IMessageSink CreateMessageHandler(IRunnerLogger logger) =>
+            new SilentReporterMessageHandler();
+    }
+}
---
+++ src/xunit.runner.reporters/SilentReporterMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xunit.Runner.Reporters
+{
+    public sealed class SilentReporterMessageHandler : LongLivedMarshalByRefObject, IMessageSinkWithTypes, IMessageSink
+    {
+        public void Dispose()
+        { }
+
+        public bool OnMessage(IMessageSinkMessage message) =>
+            true;
+
+        public bool OnMessageWithTypes(IMessageSinkMessage message, HashSet<string> messageTypes) =>
+            true;
+    }
+}
