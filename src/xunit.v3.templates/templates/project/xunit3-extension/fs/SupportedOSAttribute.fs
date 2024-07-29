1aab987feabd36489a8e423fcd148909acb8b701
Kojo Amoasi <36001967+kojo12228@users.noreply.github.com>
2024-07-29 07:05:44 +0100

    Update/fix FSharp SupportedOSAttribute project template (#2984)

--- src/xunit.v3.templates/templates/project/xunit3-extension/fs/SupportedOSAttribute.fs
+++ src/xunit.v3.templates/templates/project/xunit3-extension/fs/SupportedOSAttribute.fs
@@ -37,16 +37,19 @@ type SupportedOSAttribute([<ParamArray>]supportedOSes: SupportedOS[]) =
 
         for supportedOS in supportedOSes do
             let osPlatform =
-                match osMappings.TryGetValue supportedOS with
-                | true, value -> value
-                | _ -> raise (new ArgumentException($"Supported OS value '{supportedOS}' is not a known OS", nameof(supportedOSes)))
+                match Map.tryFind supportedOS osMappings with
+                | Some value -> value
+                | None -> invalidArg (nameof supportedOSes) $"Supported OS value '{supportedOS}' is not a known OS"
 
-            if RuntimeInformation.IsOSPlatform osPlatform then matched = true |> ignore else () 
+            if RuntimeInformation.IsOSPlatform osPlatform then
+                matched <- true
+            else
+                () 
 
             if not matched then
                 // We use the dynamic skip exception message pattern to turn this into a skipped test
                 // when it's not running on one of the targeted OSes
-                raise (new Exception($"$XunitDynamicSkip$This test is not supported on {RuntimeInformation.OSDescription}"))
+                failwith $"$XunitDynamicSkip$This test is not supported on {RuntimeInformation.OSDescription}"
             else ()
 
         Unchecked.defaultof<ValueTask>
