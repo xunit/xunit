6af136b9617a1117f9a39f8edaae7e5efd517e9b
Harshil Patel <26harshilpatel11@gmail.com>
2024-11-18 12:34:57 -0500

    added recommendation to use nameof

--- src/xunit.v3.core/Attributes/MemberDataAttributeBase.cs
+++ src/xunit.v3.core/Attributes/MemberDataAttributeBase.cs
@@ -13,7 +13,10 @@ namespace Xunit.v3;
 /// <summary>
 /// Provides a base class for attributes that will provide member data.
 /// </summary>
-/// <param name="memberName">The name of the public static member on the test class that will provide the test data</param>
+/// <param name="memberName">
+/// The name of the public static member on the test class that will provide the test data
+/// It is recommended to use the <c>nameof</c> operator to ensure compile-time safety, e.g., <c>nameof(SomeMemberName)</c>.
+/// </param>
 /// <param name="arguments">The arguments to be passed to the member (only supported for methods; ignored for everything else)</param>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
 public abstract class MemberDataAttributeBase(
