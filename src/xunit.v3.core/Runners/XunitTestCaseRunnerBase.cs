4a3cd7bbd489e22d43dea5db23146a92117eaeb6
Brad Wilson <dotnetguy@gmail.com>
2024-11-27 12:13:31 -0800

    Incorrect generics in XunitTestCaseRunnerBase

--- src/xunit.v3.core/Runners/XunitTestCaseRunnerBase.cs
+++ src/xunit.v3.core/Runners/XunitTestCaseRunnerBase.cs
@@ -4,7 +4,7 @@ namespace Xunit.v3;
 /// The test case runner for xUnit.net v3 tests (with overridable context).
 /// </summary>
 public abstract class XunitTestCaseRunnerBase<TContext, TTestCase, TTest> :
-	TestCaseRunner<XunitTestCaseRunnerContext, IXunitTestCase, IXunitTest>
+	TestCaseRunner<TContext, TTestCase, TTest>
 		where TContext : XunitTestCaseRunnerBaseContext<TTestCase, TTest>
 		where TTestCase : class, IXunitTestCase
 		where TTest : class, IXunitTest
