97239ae39d1b6b5d530f8b406413a619ef7103f3
Brad Wilson <dotnetguy@gmail.com>
2024-11-28 19:30:28 -0800

    Add a public version of DynamicSkipToken in Xunit.v3

---
+++ src/xunit.v3.core/DynamicSkipToken.cs
@@ -0,0 +1,16 @@
+namespace Xunit.v3;
+
+/// <summary>
+/// Container class for token used to dynamically skip tests.
+/// </summary>
+public static class DynamicSkipToken
+{
+	/// <summary>
+	/// The contract for exceptions which indicate that something should be skipped rather than
+	/// failed is that exception message should start with this, and that any text following this
+	/// will be treated as the skip reason (for example,
+	/// "$XunitDynamicSkip$This code can only run on Linux") will result in a skipped test with
+	/// the reason of "This code can only run on Linux".
+	/// </summary>
+	public const string Value = "$XunitDynamicSkip$";
+}
