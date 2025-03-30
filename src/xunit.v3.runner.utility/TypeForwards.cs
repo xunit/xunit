74230979ed1d96848bda8c055946376e4a909843
Brad Wilson <dotnetguy@gmail.com>
2025-03-30 13:29:20 -0700

    Move front controller data structures to xunit.v3.runner.common

---
+++ src/xunit.v3.runner.utility/TypeForwards.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+using Xunit;
+
+[assembly: TypeForwardedTo(typeof(FrontControllerFindAndRunSettings))]
+[assembly: TypeForwardedTo(typeof(FrontControllerFindSettings))]
+[assembly: TypeForwardedTo(typeof(FrontControllerLaunchOptions))]
+[assembly: TypeForwardedTo(typeof(FrontControllerRunSettings))]
+[assembly: TypeForwardedTo(typeof(FrontControllerSettingsBase))]
+[assembly: TypeForwardedTo(typeof(IFrontController))]
+[assembly: TypeForwardedTo(typeof(IFrontControllerDiscoverer))]
