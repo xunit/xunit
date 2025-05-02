c5bde2421b08255cbf09d4ed9354c3e568b0067e
Brad Wilson <dotnetguy@gmail.com>
2025-05-02 16:44:20 -0700

    #3284: Make ITypeAwareDataAttribute public

--- src/xunit.v3.core/Abstractions/Attributes/ITypeAwareDataAttribute.cs
+++ src/xunit.v3.core/Abstractions/Attributes/ITypeAwareDataAttribute.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Reflection;
 
 namespace Xunit.v3;
 
-// Used to tag data attributes that want to know the type they originated from
-internal interface ITypeAwareDataAttribute
+/// <summary>
+/// This interface supplements <see cref="IDataAttribute"/> for attributes that wish to be
+/// notified of the <see cref="MemberInfo.ReflectedType"/> of the method that was used during
+/// reflection that they were attached to.
+/// </summary>
+/// <remarks>
+/// By default, this is called by <see cref="ExtensibilityPointFactory.GetMethodDataAttributes"/>.
+/// If a third party test framework uses some other mechanism to discover data attributes that are
+/// attached to methods, it should conditionally populate the <see cref="MemberType"/> during its
+/// own discovery phase.
+/// </remarks>
+public interface ITypeAwareDataAttribute
 {
+	/// <summary>
+	/// Gets or sets the reflected type of the method that this data attribute was attached to.
+	/// </summary>
+	/// <remarks>
+	/// <see cref="ExtensibilityPointFactory"/> will not be overwrite this value if it's already non-<c>null</c>.
+	/// </remarks>
 	Type? MemberType { get; set; }
 }
