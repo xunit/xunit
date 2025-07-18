995836ad5bfd717b11974d85d7c5eb41ddc7b21d
Brad Wilson <dotnetguy@gmail.com>
2025-07-17 17:29:04 -0700

    Push IAsyncDisposable.SafeDisposeAsync into the correct namespace

--- src/common/DisposableExtensions.cs
+++ src/common/DisposableExtensions.cs
@@ -1,25 +1,30 @@
-using System.Threading.Tasks;
-
-namespace System;
-
-internal static class DisposableExtensions
+namespace System
 {
-	public static void SafeDispose(this IDisposable? disposable)
+	internal static class DisposableExtensions
 	{
-		try
+		public static void SafeDispose(this IDisposable? disposable)
 		{
-			disposable?.Dispose();
-		}
-		catch { }
-	}
-
-	public static async ValueTask SafeDisposeAsync(this IAsyncDisposable? asyncDisposable)
-	{
-		if (asyncDisposable is not null)
 			try
 			{
-				await asyncDisposable.DisposeAsync();
+				disposable?.Dispose();
 			}
 			catch { }
+		}
+	}
+}
+
+namespace System.Threading.Tasks
+{
+	internal static class AsyncDisposableExtensions
+	{
+		public static async ValueTask SafeDisposeAsync(this IAsyncDisposable? asyncDisposable)
+		{
+			if (asyncDisposable is not null)
+				try
+				{
+					await asyncDisposable.DisposeAsync();
+				}
+				catch { }
+		}
 	}
 }
