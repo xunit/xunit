9576514386c09a0ef8615c5c0a09811c506ec5d5
Brad Wilson <dotnetguy@gmail.com>
2025-09-15 15:51:18 -0700

    Binary log integration

--- tools/builder/targets/Build.cs
+++ tools/builder/targets/Build.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit.BuildTools.Models;
 
@@ -7,8 +9,17 @@ public static partial class Build
 {
 	public static partial async Task PerformBuild(BuildContext context)
 	{
+		context.BuildStep("Listing folders in C:\\Program Files\\dotnet\\packs\\Microsoft.NETCore.App.Ref");
+
+		foreach (var directory in Directory.GetDirectories("C:\\Program Files\\dotnet\\packs\\Microsoft.NETCore.App.Ref"))
+			Console.WriteLine("  {0}", directory);
+
+		Console.WriteLine();
+
 		context.BuildStep("Compiling binaries");
 
-		await context.Exec("dotnet", $"msbuild -nologo -maxCpuCount -restore:False -verbosity:{context.Verbosity} -p:Configuration={context.ConfigurationText}");
+		var buildLog = Path.Combine(context.BuildArtifactsFolder, "build.binlog");
+
+		await context.Exec("dotnet", $"msbuild -nologo -maxCpuCount -restore:False -verbosity:{context.Verbosity} -property:Configuration={context.ConfigurationText} -binaryLogger:{buildLog}");
 	}
 }
