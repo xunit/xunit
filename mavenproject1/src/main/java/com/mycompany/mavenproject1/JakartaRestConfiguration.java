9564c513ee83728efcf5de8ac616baf4c77e971d
ah670 <ah670@192.168.1.172>
2025-09-27 14:10:29 -0500

    Initial commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenproject1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.mavenproject1.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
