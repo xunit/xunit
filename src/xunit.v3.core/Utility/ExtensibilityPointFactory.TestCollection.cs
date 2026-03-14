e23840f5eed5f7faada560606c2797dc246fdbae
Brad Wilson <dotnetguy@gmail.com>
2026-03-13 17:13:37 -0700

    Incorrect obsolete instruction

--- src/xunit.v3.core/Utility/ExtensibilityPointFactory.TestCollection.cs
+++ src/xunit.v3.core/Utility/ExtensibilityPointFactory.TestCollection.cs
@@ -8,7 +8,7 @@ partial class ExtensibilityPointFactory
 	/// Please call <see cref="RegisteredEngineConfig.GetCollectionTestCaseOrderer"/>.
 	/// This method will be removed in the next major version.
 	/// </summary>
-	[Obsolete("Please call RegisteredEngineConfig.GetClassTestCaseOrderer. This method will be removed in the next major version.")]
+	[Obsolete("Please call RegisteredEngineConfig.GetCollectionTestCaseOrderer. This method will be removed in the next major version.")]
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	public static ITestCaseOrderer? GetCollectionTestCaseOrderer(Type? collectionDefinition) =>
 		RegisteredEngineConfig.GetCollectionTestCaseOrderer(collectionDefinition);
