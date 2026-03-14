9e61839dabbfe1fafa798df0b874518b8bb51f67
Brad Wilson <dotnetguy@gmail.com>
2026-03-13 17:15:09 -0700

    Un-obsolete FrontControllerRunSettings.SerializedTestCases

--- src/xunit.v3.runner.common/Frameworks/FrontControllerRunSettings.cs
+++ src/xunit.v3.runner.common/Frameworks/FrontControllerRunSettings.cs
@@ -24,9 +24,7 @@ public partial class FrontControllerRunSettings : FrontControllerSettingsBase
 		IReadOnlyCollection<string> testCaseIDsToRun)
 	{
 		Options = Guard.ArgumentNotNull(options);
-#if !XUNIT_AOT
 		SerializedTestCases = Guard.ArgumentNotNull(serializedTestCases);
-#endif
 		TestCaseIDsToRun = Guard.ArgumentNotNull(testCaseIDsToRun);
 	}
 
@@ -36,9 +34,38 @@ public partial class FrontControllerRunSettings : FrontControllerSettingsBase
 	public ITestFrameworkExecutionOptions Options { get; }
 
 	/// <summary>
+	/// Get the list of test cases to be run.
+	/// </summary>
+	public IReadOnlyCollection<string> SerializedTestCases { get; }
+
+	/// <summary>
 	/// Get the list of test case IDs to be run.
 	/// </summary>
 	public IReadOnlyCollection<string> TestCaseIDsToRun { get; }
+
+	/// <summary>
+	/// Creates a new instance of <see cref="FrontControllerRunSettings"/> to run a set of test cases
+	/// by serialization.
+	/// </summary>
+	/// <param name="options">The options to use during execution</param>
+	/// <param name="serializedTestCases">The test cases to run</param>
+	public static FrontControllerRunSettings WithSerializedTestCases(
+		ITestFrameworkExecutionOptions options,
+		IReadOnlyCollection<string> serializedTestCases) =>
+			new(options, serializedTestCases, []);
+
+	/// <summary>
+	/// Creates a new instance of <see cref="FrontControllerRunSettings"/> to run a set of test cases
+	/// by serialization and a set of test cases by test case ID.
+	/// </summary>
+	/// <param name="options">The options to use during execution</param>
+	/// <param name="serializedTestCases">The test cases to run</param>
+	/// <param name="testCaseIDsToRun">The test case IDs to run</param>
+	public static FrontControllerRunSettings WithSerializedTestCasesAndTestCaseIDs(
+		ITestFrameworkExecutionOptions options,
+		IReadOnlyCollection<string> serializedTestCases,
+		IReadOnlyCollection<string> testCaseIDsToRun) =>
+			new(options, serializedTestCases, testCaseIDsToRun);
 
 	/// <summary>
 	/// Creates a new instance of <see cref="FrontControllerRunSettings"/> to run a set of test cases
